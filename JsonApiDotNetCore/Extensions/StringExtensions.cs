fd9f883e9217cc1ea6600626b4ef7558ecda3c38
Jared Nance <jaredcnance@gmail.com>
2016-08-24 11:51:12 -0500

    add camel case extension

---
+++ JsonApiDotNetCore/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace JsonApiDotNetCore.Extensions
+{
+  public static class StringExtensions
+  {
+    public static string ToCamelCase(this string str)
+    {
+      var splittedPhrase = str.Split(' ', '-', '.');
+      var sb = new StringBuilder();
+      foreach (var s in splittedPhrase)
+      {
+        var splittedPhraseChars = s.ToCharArray();
+        if (splittedPhraseChars.Length > 0)
+        {
+          splittedPhraseChars[0] = new string(splittedPhraseChars[0], 1).ToUpper().ToCharArray()[0];
+        }
+        sb.Append(new string(splittedPhraseChars));
+      }
+      return sb.ToString();
+    }
+  }
+}
