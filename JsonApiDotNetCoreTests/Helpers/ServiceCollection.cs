4dca3463f84c94f5d0d582044a967eba99cc0614
Jared Nance <jaredcnance@gmail.com>
2016-08-26 20:35:08 -0500

    inherit List for service collection

--- JsonApiDotNetCoreTests/Helpers/ServiceCollection.cs
+++ JsonApiDotNetCoreTests/Helpers/ServiceCollection.cs
@@ -1,64 +1,14 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace JsonApiDotNetCoreTests
+namespace JsonApiDotNetCoreTests.Helpers
 {
-  public class ServiceCollection : IServiceCollection
+  public class ServiceCollection : List<ServiceDescriptor>, IServiceCollection
   {
-    public int IndexOf(ServiceDescriptor serviceDescriptor)
+    IEnumerator IEnumerable.GetEnumerator()
     {
-      throw new NotImplementedException();
+      return GetEnumerator();
     }
-
-    public void Insert(int pos, ServiceDescriptor serviceDescriptor)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void RemoveAt(int pos)
-    {
-      throw new NotImplementedException();
-    }
-
-    public bool Remove(ServiceDescriptor serviceDescriptor)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void Add(ServiceDescriptor serviceDescriptor)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void Clear()
-    {
-      throw new NotImplementedException();
-    }
-
-    public bool Contains(ServiceDescriptor serviceDescriptor)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void CopyTo(ServiceDescriptor[] serviceDescriptor, int pos)
-    {
-      throw new NotImplementedException();
-    }
-
-    public IEnumerable<ServiceDescriptor>.IEnumerator<ServiceDescriptor> GetEnumerator()
-    {
-      throw new NotImplementedException();
-    }
-
-    public IEnumerator GetEnumerator()
-    {
-      throw new NotImplementedException();
-    }
-
-    public int Count { get; set; }
-    public bool IsReadOnly { get; set; }
-
   }
 }
