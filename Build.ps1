6d5b4497f58a026947c97c232b06f1eecbac37e1
Jared Nance <jaredcnance@gmail.com>
2017-02-11 17:27:01 -0600

    fix(ci): Invoke-MSBuild fails since there is not project.json

--- Build.ps1
+++ Build.ps1
@@ -54,10 +54,13 @@ EnsurePsbuildInstalled
 
 exec { & dotnet restore }
 
-Invoke-MSBuild
+#Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+
+exec { & dotnet build .\src\JsonApiDotNetCore -c Release }
+exec { & dotnet build .\test\JsonApiDotNetCoreExampleTests -c Release }
 
 exec { & dotnet test .\test\JsonApiDotNetCoreExampleTests -c Release }
 
