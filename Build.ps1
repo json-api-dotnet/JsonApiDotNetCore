74594a459c70b7e1f54a978356f5fb69a18e7a96
Jared Nance <jaredcnance@gmail.com>
2017-03-14 08:28:30 -0500

    fix(build.ps1): remove obsolete exec cmd

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,6 @@
-exec { & dotnet restore .\src\JsonApiDotNetCore\JsonApiDotNetCore.csproj }
-
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet build .\src\JsonApiDotNetCore -c Release }
-
-exec { & dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision }
+dotnet restore .\src\JsonApiDotNetCore\JsonApiDotNetCore.csproj
+dotnet build .\src\JsonApiDotNetCore -c Release
+dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision
