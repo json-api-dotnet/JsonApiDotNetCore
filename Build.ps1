26b04e33462d6ea434d9d574fb89993270a5e4e6
Jared Nance <jaredcnance@gmail.com>
2017-03-15 17:07:02 -0500

    debug(appveyor): log the command being executed
    the search continues

--- Build.ps1
+++ Build.ps1
@@ -7,5 +7,11 @@ dotnet build .\src\JsonApiDotNetCore -c Release
 echo "APPVEYOR_REPO_TAG: $env:APPVEYOR_REPO_TAG"
 echo "VERSION-SUFFIX: alpha1-$revision"
 
-If($env:APPVEYOR_REPO_TAG) { dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts }
-Else { dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision }
+If($env:APPVEYOR_REPO_TAG) {
+    echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts "
+    dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts 
+}
+Else { 
+    echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision"
+    dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision 
+}
