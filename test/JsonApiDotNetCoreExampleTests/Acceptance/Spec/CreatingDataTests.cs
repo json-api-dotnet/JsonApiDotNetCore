cedea74eb950ea09789df5eb68a80b3313df170a
jcnance <jaredcnance@gmail.com>
2017-03-15 10:18:14 -0500

    test(creating-data): can create new entities with hasMany relationships

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/CreatingDataTests.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/CreatingDataTests.cs
@@ -14,6 +14,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
 {
@@ -55,16 +58,16 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             {
                 data = new
                 {
-                    type = "todo-item-collections"
-                },
-                relationships = new
-                {
-                    owner = new
+                    type = "todo-item-collections",
+                    relationships = new
                     {
-                        data = new
+                        owner = new
                         {
-                            type = "people",
-                            id = owner.Id.ToString()
+                            data = new
+                            {
+                                type = "people",
+                                id = owner.Id.ToString()
+                            }
                         }
                     }
                 }
@@ -115,6 +118,73 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
         }
 
         [Fact]
+        public async Task Can_Create_And_Set_HasMany_Relationships()
+        {
+            // arrange
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            var httpMethod = new HttpMethod("POST");
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+
+            var context = _fixture.GetService<AppDbContext>();
+
+            var owner = new JsonApiDotNetCoreExample.Models.Person();
+            var todoItem = new TodoItem();
+            todoItem.Owner = owner;
+            context.People.Add(owner);
+            context.TodoItems.Add(todoItem);
+            await context.SaveChangesAsync();
+
+            var route = "/api/v1/todo-item-collections";
+            var request = new HttpRequestMessage(httpMethod, route);
+            var content = new
+            {
+                data = new
+                {
+                    type = "todo-item-collections",
+                    relationships = new Dictionary<string, dynamic>
+                    {
+                        {  "owner",  new {
+                            data = new
+                            {
+                                type = "people",
+                                id = owner.Id.ToString()
+                            }
+                        } },
+                        {  "todo-items", new {
+                            data = new dynamic[]
+                            {
+                                new {
+                                    type = "todo-items",
+                                    id = todoItem.Id.ToString()
+                                }
+                            }
+                        } }
+                    }
+                }
+            };
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(content));
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.api+json");
+            
+            // act
+            var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            var deserializedBody = (TodoItemCollection)JsonApiDeSerializer.Deserialize(body, _jsonApiContext, context);
+            var newId = deserializedBody.Id;
+            var contextCollection = context.TodoItemCollections
+                .Include(c=> c.Owner)
+                .Include(c => c.TodoItems)
+                .SingleOrDefault(c => c.Id == newId);
+
+            // assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(owner.Id, contextCollection.OwnerId);
+            Assert.NotEmpty(contextCollection.TodoItems);
+        }
+
+        [Fact]
         public async Task ShouldReceiveLocationHeader_InResponse()
         {
             // arrange
@@ -144,7 +214,7 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             // act
             var response = await client.SendAsync(request);
             var body = await response.Content.ReadAsStringAsync();
-            var deserializedBody = (TodoItem)JsonApiDeSerializer.Deserialize(body, _jsonApiContext);
+            var deserializedBody = (TodoItem)JsonApiDeSerializer.Deserialize(body, _jsonApiContext, _fixture.GetService<AppDbContext>());
 
             // assert
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
