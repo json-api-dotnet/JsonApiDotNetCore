171b9d2d5d1f4f374400e6abd68f900c696ff8a3
Jared Nance <jaredcnance@gmail.com>
2017-03-15 16:53:33 -0500

    debug(build.ps1): check what the value of APPVEYOR_REPO_TAG is

--- Build.ps1
+++ Build.ps1
@@ -4,5 +4,7 @@ $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 dotnet restore .\src\JsonApiDotNetCore\JsonApiDotNetCore.csproj
 dotnet build .\src\JsonApiDotNetCore -c Release
 
+echo "APPVEYOR_REPO_TAG: $env:APPVEYOR_REPO_TAG"
+
 If($env:APPVEYOR_REPO_TAG) { dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts }
 Else { dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision }
