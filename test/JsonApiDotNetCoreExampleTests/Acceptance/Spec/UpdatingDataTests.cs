588b98036c79d266f32f6dda0dc876123dbd7a4f
Jared Nance <jaredcnance@gmail.com>
2017-03-20 10:50:02 -0500

    test(patch): failing test for patching entity has one relationship

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/UpdatingDataTests.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/UpdatingDataTests.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,8 +6,6 @@ using System.Threading.Tasks;
 using Bogus;
 using DotNetCoreDocs;
 using DotNetCoreDocs.Writers;
-using JsonApiDotNetCore.Serialization;
-using JsonApiDotNetCore.Services;
 using JsonApiDotNetCoreExample;
 using JsonApiDotNetCoreExample.Data;
 using JsonApiDotNetCoreExample.Models;
@@ -16,6 +13,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
 using Xunit;
+using Person = JsonApiDotNetCoreExample.Models.Person;
 
 namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
 {
@@ -25,14 +23,18 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
         private DocsFixture<Startup, JsonDocWriter> _fixture;
         private AppDbContext _context;
         private Faker<TodoItem> _todoItemFaker;
+        private Faker<Person> _personFaker;
 
         public UpdatingDataTests(DocsFixture<Startup, JsonDocWriter> fixture)
         {
             _fixture = fixture;
             _context = fixture.GetService<AppDbContext>();
-             _todoItemFaker = new Faker<TodoItem>()
+            _todoItemFaker = new Faker<TodoItem>()
                 .RuleFor(t => t.Description, f => f.Lorem.Sentence())
                 .RuleFor(t => t.Ordinal, f => f.Random.Number());
+            _personFaker = new Faker<Person>()
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName());
         }
 
         [Fact]
@@ -72,6 +74,62 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
 
             // Assert
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Can_Patch_Entity_And_HasOne_Relationships()
+        {
+            // arrange
+            var todoItem = _todoItemFaker.Generate();
+            var person = _personFaker.Generate();
+            _context.TodoItems.Add(todoItem);
+            _context.People.Add(person);
+            _context.SaveChanges();
+
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+            
+            var content = new
+            {
+                data = new
+                {
+                    type = "todo-items",
+                    attributes = new
+                    {
+                        description = todoItem.Description,
+                        ordinal = todoItem.Ordinal
+                    },
+                    relationships = new
+                    {
+                        owner = new
+                        {
+                            data = new
+                            {
+                                type = "people",
+                                id = person.Id.ToString()
+                            }
+                        }
+                    }
+                }
+            };
+
+            var httpMethod = new HttpMethod("PATCH");
+            var route = $"/api/v1/todo-items/{todoItem.Id}";
+            var request = new HttpRequestMessage(httpMethod, route);
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(content));
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.api+json");
+
+            // Act
+            var response = await client.SendAsync(request);
+            var updatedTodoItem = _context.TodoItems
+                .SingleOrDefault(t => t.Id == todoItem.Id);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(person.Id, updatedTodoItem.OwnerId);
         }
     }
 }
