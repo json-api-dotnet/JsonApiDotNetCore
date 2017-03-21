743edd13e2d907d77117f5923cabda7250f32382
Jared Nance <jaredcnance@gmail.com>
2017-03-20 21:51:43 -0500

    test(sparse-fields): test that fields can be restricted using query

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/SparseFieldSetTests.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/SparseFieldSetTests.cs
@@ -8,6 +8,12 @@ using JsonApiDotNetCoreExample.Data;
 using JsonApiDotNetCore.Extensions;
 using JsonApiDotNetCoreExample.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using JsonApiDotNetCore.Models;
+using System.Diagnostics;
 
 namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
 {
@@ -45,6 +51,37 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             // assert
             Assert.Equal(0, result.Ordinal);
             Assert.Equal(todoItem.Description, result.Description);
+        }
+
+        [Fact]
+        public async Task Fields_Query_Selects_Sparse_Field_Sets()
+        {
+            // arrange
+            var todoItem = new TodoItem {
+                Description = "description",
+                Ordinal = 1
+            };
+            _dbContext.TodoItems.Add(todoItem);
+            await _dbContext.SaveChangesAsync();
+            
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            var httpMethod = new HttpMethod("GET");
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+
+            var route = $"/api/v1/todo-items/{todoItem.Id}?fields[todo-items]=description";
+            var request = new HttpRequestMessage(httpMethod, route);
+
+            // act
+            var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            var deserializeBody = JsonConvert.DeserializeObject<Document>(body);
+
+            // assert
+            Assert.Equal(todoItem.StringId, deserializeBody.Data.Id);
+            Assert.Equal(1, deserializeBody.Data.Attributes.Count);
+            Assert.Equal(todoItem.Description, deserializeBody.Data.Attributes["description"]);
         }
     }
 }
