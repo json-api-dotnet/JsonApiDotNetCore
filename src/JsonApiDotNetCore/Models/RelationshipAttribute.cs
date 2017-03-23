b02d85c64d174bee033ab6bd70ba7c75ef1ccc88
jfhs <romoper@yandex.ru>
2017-03-23 20:09:01 +0100

    fix(RelationshipAttribute): correct Equals and ToString

--- src/JsonApiDotNetCore/Models/RelationshipAttribute.cs
+++ src/JsonApiDotNetCore/Models/RelationshipAttribute.cs
@@ -16,5 +16,20 @@ namespace JsonApiDotNetCore.Models
         public bool IsHasOne { get { return this.GetType() == typeof(HasOneAttribute); } }
 
         public abstract void SetValue(object entity, object newValue);
+
+        public override string ToString()
+        {
+            return base.ToString() + ":" + PublicRelationshipName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            RelationshipAttribute attr = obj as RelationshipAttribute;
+            if (attr == null)
+            {
+                return false;
+            }
+            return IsHasMany == attr.IsHasMany && PublicRelationshipName.Equals(attr.PublicRelationshipName);
+        }
     }
 }
