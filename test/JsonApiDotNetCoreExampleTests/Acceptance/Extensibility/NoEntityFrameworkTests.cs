93edba56363eae8681d797f78a0668f68cf74192
jaredcnance <jaredcnance@gmail.com>
2017-04-18 14:45:32 -0500

    test(acceptance/no-ef): ensure the database is created
    this should be cleaned up by #91

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Extensibility/NoEntityFrameworkTests.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Extensibility/NoEntityFrameworkTests.cs
@@ -8,20 +8,39 @@ using JsonApiDotNetCoreExampleTests.Helpers.Extensions;
 using System.Threading.Tasks;
 using System.Net;
 using JsonApiDotNetCoreExample.Models;
+using JsonApiDotNetCoreExample.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace JsonApiDotNetCoreExampleTests.Acceptance.Extensibility
 {
     public class NoEntityFrameworkTests
     {
+        private readonly TestServer _server;
+        private readonly IConfiguration _config;
+        private readonly AppDbContext _context;
+
+        public NoEntityFrameworkTests()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            _server = new TestServer(builder);
+            _config = _server.GetService<IConfiguration>();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql(_config.GetValue<string>("Data:DefaultConnection"));
+            _context = new AppDbContext(optionsBuilder.Options);
+            _context.Database.EnsureCreated();
+        }
+
         [Fact]
         public async Task Can_Implement_Custom_IResourceService_Without_EFAsync()
         {
             // arrange
-            var builder = new WebHostBuilder()
-                .UseStartup<Startup>();
+            _context.TodoItems.Add(new TodoItem());
+            _context.SaveChanges();
 
-            var server = new TestServer(builder);
-            var client = server.CreateClient();
+            var client = _server.CreateClient();
 
             var httpMethod = new HttpMethod("GET");
             var route = $"/api/v1/custom-todo-items";
@@ -31,7 +50,7 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Extensibility
             // act
             var response = await client.SendAsync(request);
             var responseBody = await response.Content.ReadAsStringAsync();
-            var deserializedBody = server.GetService<IJsonApiDeSerializer>()
+            var deserializedBody = _server.GetService<IJsonApiDeSerializer>()
                 .DeserializeList<TodoItem>(responseBody);
 
             // assert
