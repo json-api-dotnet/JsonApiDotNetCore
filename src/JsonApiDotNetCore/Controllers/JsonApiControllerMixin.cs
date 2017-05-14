38f15c3faf5519b00d7fad8fb35cf22f412644ce
jaredcnance <jaredcnance@gmail.com>
2017-05-13 21:11:36 -0500

    feat(controllers): add errors response

--- src/JsonApiDotNetCore/Controllers/JsonApiControllerMixin.cs
+++ src/JsonApiDotNetCore/Controllers/JsonApiControllerMixin.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using JsonApiDotNetCore.Internal;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JsonApiDotNetCore.Controllers
@@ -12,6 +14,27 @@ namespace JsonApiDotNetCore.Controllers
         protected IActionResult Forbidden()
         {
             return new StatusCodeResult(403);
+        }
+
+        protected IActionResult Errors(ErrorCollection errors)
+        {
+            var result = new ObjectResult(errors);
+            result.StatusCode = GetErrorStatusCode(errors);
+
+            return result;
+        }
+
+        private int GetErrorStatusCode(ErrorCollection errors) 
+        {
+            var statusCodes = errors.Errors
+                .Select(e => (int)e.StatusCode)
+                .Distinct()
+                .ToList();
+
+            if(statusCodes.Count == 1)
+                return statusCodes[0];
+            
+            return int.Parse(statusCodes.Max().ToString()[0] + "00");
         }
     }
 }
