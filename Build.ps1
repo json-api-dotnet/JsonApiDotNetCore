a8966ac5edb663be487027a93f0ba5eac44f7fbf
jaredcnance <jaredcnance@gmail.com>
2017-07-02 18:20:17 -0500

    fix build script

--- Build.ps1
+++ Build.ps1
@@ -6,7 +6,7 @@ function Get-Version-Suffix-From-Tag
   $split=$tag -split "-"
   $suffix=$split[1..2]
   $final=$suffix -join "-"
-  return $c
+  return $final
 }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
@@ -21,14 +21,16 @@ dotnet test ./test/NoEntityFrameworkTests/NoEntityFrameworkTests.csproj
 dotnet build .\src\JsonApiDotNetCore -c Release
 
 echo "APPVEYOR_REPO_TAG: $env:APPVEYOR_REPO_TAG"
-echo "VERSION-SUFFIX: alpha1-$revision"
+
 
 If($env:APPVEYOR_REPO_TAG -eq $true) {
-    echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts "
     $revision = Get-Version-Suffix-From-Tag
+    echo "VERSION-SUFFIX: $revision"
+    echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision"
     dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision 
 }
 Else { 
+    echo "VERSION-SUFFIX: alpha1-$revision"
     echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision"
     dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision 
 }
