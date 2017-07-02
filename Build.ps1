34aabb6ae498b8ba1c2110a42e1f0e2ec21581c0
jaredcnance <jaredcnance@gmail.com>
2017-07-02 18:09:32 -0500

    fix build script

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,14 @@
+# Gets the version suffix from the repo tag
+# example: v1.0.0-preview1-final => preview1-final
+function Get-Version-Suffix-From-Tag
+{
+  $tag=$env:APPVEYOR_REPO_TAG_NAME
+  $split=$tag -split "-"
+  $suffix=$split[1..2]
+  $final=$suffix -join "-"
+  return $c
+}
+
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
@@ -15,20 +26,9 @@ echo "VERSION-SUFFIX: alpha1-$revision"
 If($env:APPVEYOR_REPO_TAG -eq $true) {
     echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts "
     $revision = Get-Version-Suffix-From-Tag
-    dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision 
+    dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision 
 }
 Else { 
     echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision"
     dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision 
-}
-
-# Gets the version suffix from the repo tag
-# example: v1.0.0-preview1-final => preview1-final
-function Get-Version-Suffix-From-Tag
-{
-  $tag=$env:APPVEYOR_REPO_TAG_NAME
-  $split=$tag -split "-"
-  $suffix=$split[1..2]
-  $final=$suffix -join "-"
-  return $c
 }
