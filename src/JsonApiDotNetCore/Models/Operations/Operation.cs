fc161d2708fbfcd6256c74b218c99c5a28506700
jaredcnance <jaredcnance@gmail.com>
2017-08-09 22:16:01 -0500

    feat(models): initial operation models

---
+++ src/JsonApiDotNetCore/Models/Operations/Operation.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using JsonApiDotNetCore.Controllers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonApiDotNetCore.Models.Operations
+{
+    public class Operation : DocumentBase
+    {
+        [JsonProperty("op")]
+        public OperationCode Op { get; set; }
+
+        [JsonProperty("ref")]
+        public ResourceReference Ref { get; set; }
+
+        [JsonProperty("params")]
+        public QueryParams Params { get; set; }
+
+        [JsonProperty("data")]
+        public object Data
+        {
+            get
+            {
+                if (DataIsList) return DataList;
+                return DataObject;
+            }
+            set => SetData(value);
+        }
+
+        private void SetData(object data)
+        {
+            if (data is JArray jArray) {
+                DataIsList = true;
+                DataList = jArray.ToObject<List<DocumentData>>();
+            }
+            else if (data is List<DocumentData> dataList) {
+                DataIsList = true;
+                DataList = dataList;
+            }
+            else if (data is JObject jObject) {
+                DataObject = jObject.ToObject<DocumentData>();
+            }
+            else if (data is DocumentData dataObject) {
+                DataObject = dataObject;
+            }
+        }
+
+        public bool DataIsList { get; private set; }
+        public List<DocumentData> DataList { get; private set; }
+        public DocumentData DataObject { get; private set; }
+    }
+}
