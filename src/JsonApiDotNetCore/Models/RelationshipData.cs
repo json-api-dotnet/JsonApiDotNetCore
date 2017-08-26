63827cd3f77db64c39777dac8698011291de1330
jaredcnance <jaredcnance@gmail.com>
2017-08-25 23:44:27 -0500

    feat(RelationshipData): expose IsHasMany property

--- src/JsonApiDotNetCore/Models/RelationshipData.cs
+++ src/JsonApiDotNetCore/Models/RelationshipData.cs
@@ -11,29 +11,41 @@ namespace JsonApiDotNetCore.Models
         public Links Links { get; set; }
 
         [JsonProperty("data")]
-        public object ExposedData { 
-            get {
-                if(ManyData != null)
+        public object ExposedData
+        {
+            get
+            {
+                if (ManyData != null)
                     return ManyData;
                 return SingleData;
             }
-            set {
-                if(value is IEnumerable)
-                    if(value is JObject jObject)
-                        SingleData = jObject.ToObject<Dictionary<string, object>>();   
-                    else if(value is JArray jArray)
-                        ManyData = jArray.ToObject<List<Dictionary<string, object>>>();
-                    else
-                        ManyData = (List<Dictionary<string, object>>)value;
-                else
+            set
+            {
+                if (value is JObject jObject)
+                    SingleData = jObject.ToObject<Dictionary<string, object>>();
+                else if (value is Dictionary<string, object> dict)
                     SingleData = (Dictionary<string, object>)value;
+                else
+                    SetManyData(value);
             }
-         }
+        }
+
+        private void SetManyData(object value)
+        {
+            IsHasMany = true;
+            if (value is JArray jArray)
+                ManyData = jArray.ToObject<List<Dictionary<string, object>>>();
+            else
+                ManyData = (List<Dictionary<string, object>>)value;
+        }
 
         [JsonIgnore]
         public List<Dictionary<string, object>> ManyData { get; set; }
-        
+
         [JsonIgnore]
         public Dictionary<string, object> SingleData { get; set; }
+
+        [JsonIgnore]
+        public bool IsHasMany { get; private set; }
     }
 }
---
+++ test/UnitTests/Models/RelationshipDataTests.cs
@@ -0,0 +1,90 @@
+﻿using JsonApiDotNetCore.Models;
+using System.Collections.Generic;
+using Xunit;
+using Newtonsoft.Json.Linq;
+
+namespace UnitTests.Models
+{
+    public class RelationshipDataTests
+    {
+        [Fact]
+        public void Setting_ExposedData_To_List_Sets_ManyData()
+        {
+            // arrange
+            var relationshipData = new RelationshipData();
+            var relationships = new List<Dictionary<string, object>> {
+                new Dictionary<string, object> {
+                    { "authors", new { } }
+                }
+            };
+
+            // act 
+            relationshipData.ExposedData = relationships;
+
+            // assert
+            Assert.NotEmpty(relationshipData.ManyData);
+            Assert.True(relationshipData.ManyData[0].ContainsKey("authors"));
+            Assert.True(relationshipData.IsHasMany);
+        }
+
+        [Fact]
+        public void Setting_ExposedData_To_JArray_Sets_ManyData()
+        {
+            // arrange
+            var relationshipData = new RelationshipData();
+            var relationshipsJson = @"[
+                {
+                    ""authors"": {}
+                }
+            ]";
+
+            var relationships = JArray.Parse(relationshipsJson);
+
+            // act 
+            relationshipData.ExposedData = relationships;
+
+            // assert
+            Assert.NotEmpty(relationshipData.ManyData);
+            Assert.True(relationshipData.ManyData[0].ContainsKey("authors"));
+            Assert.True(relationshipData.IsHasMany);
+        }
+
+        [Fact]
+        public void Setting_ExposedData_To_Dictionary_Sets_SingleData()
+        {
+            // arrange
+            var relationshipData = new RelationshipData();
+            var relationship = new Dictionary<string, object> {
+                { "authors", new { } }
+            };
+
+            // act 
+            relationshipData.ExposedData = relationship;
+
+            // assert
+            Assert.NotNull(relationshipData.SingleData);
+            Assert.True(relationshipData.SingleData.ContainsKey("authors"));
+            Assert.False(relationshipData.IsHasMany);
+        }
+
+        [Fact]
+        public void Setting_ExposedData_To_JObject_Sets_SingleData()
+        {
+            // arrange
+            var relationshipData = new RelationshipData();
+            var relationshipJson = @"{
+                    ""authors"": {}
+                }";
+
+            var relationship = JObject.Parse(relationshipJson);
+
+            // act 
+            relationshipData.ExposedData = relationship;
+
+            // assert
+            Assert.NotNull(relationshipData.SingleData);
+            Assert.True(relationshipData.SingleData.ContainsKey("authors"));
+            Assert.False(relationshipData.IsHasMany);
+        }
+    }
+}
