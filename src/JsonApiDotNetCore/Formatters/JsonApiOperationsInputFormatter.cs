8e6a2401198ce84f2f12f8b5c8ed071a74ca9101
jaredcnance <jaredcnance@gmail.com>
2017-08-26 21:11:58 -0500

    feat(content-negotiation): wire up reader and input formatter

---
+++ src/JsonApiDotNetCore/Formatters/JsonApiOperationsInputFormatter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+
+namespace JsonApiDotNetCore.Formatters
+{
+    public class JsonApiOperationsInputFormatter : IInputFormatter
+    {
+        const string PROFILE_EXTENSION = "<http://example.org/profiles/myjsonstuff>; rel=\"profile\"";
+
+        public bool CanRead(InputFormatterContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var contentTypeString = context.HttpContext.Request.ContentType;
+
+            return (
+                contentTypeString == "application/vnd.api+json" && 
+                context.HttpContext.Request.Headers.TryGetValue("Link", out StringValues profileExtension) &&
+                profileExtension == PROFILE_EXTENSION
+            );
+        }
+
+        public async Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            var reader = context.HttpContext.RequestServices.GetService<IJsonApiOperationsReader>();
+            return await reader.ReadAsync(context);
+        }
+    }
+}
