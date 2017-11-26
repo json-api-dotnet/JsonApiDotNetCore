c1ca4752941979c5ec7059f8ef774eda07609b17
jaredcnance <jaredcnance@gmail.com>
2017-11-26 11:16:57 -0600

    perf(QueryParser): improve sort parsing perf
    AscendingSort: 30.9% improvement
    DescendingSort: 20% improvement
    
    BEFORE
    
              Method |     Mean |     Error |    StdDev|
    --------------- |---------:|----------:|----------:|
      AscendingSort | 4.558 us | 0.2451 us | 0.6832 us |
     DescendingSort | 4.218 us | 0.1780 us | 0.4990 us |
    
    AFTER
    
              Method |     Mean |     Error |    StdDev |
    --------------- |---------:|----------:|----------:|
      AscendingSort | 3.146 us | 0.0326 us | 0.0709 us |
     DescendingSort | 3.372 us | 0.1228 us | 0.2618 us |

--- src/JsonApiDotNetCore/Services/QueryParser.cs
+++ src/JsonApiDotNetCore/Services/QueryParser.cs
@@ -9,63 +9,52 @@ using JsonApiDotNetCore.Internal.Query;
 using JsonApiDotNetCore.Models;
 using Microsoft.AspNetCore.Http;
 
-namespace JsonApiDotNetCore.Services
-{
-    public interface IQueryParser
-    {
+namespace JsonApiDotNetCore.Services {
+    public interface IQueryParser {
         QuerySet Parse(IQueryCollection query);
     }
 
-    public class QueryParser : IQueryParser
-    {
+    public class QueryParser : IQueryParser {
         private readonly IControllerContext _controllerContext;
         private readonly JsonApiOptions _options;
 
         public QueryParser(
             IControllerContext controllerContext,
-            JsonApiOptions options)
-        {
+            JsonApiOptions options) {
             _controllerContext = controllerContext;
             _options = options;
         }
 
-        public virtual QuerySet Parse(IQueryCollection query)
-        {
+        public virtual QuerySet Parse(IQueryCollection query) {
             var querySet = new QuerySet();
-            var disabledQueries = _controllerContext.GetControllerAttribute<DisableQueryAttribute>()?.QueryParams ?? QueryParams.None;
+            var disabledQueries = _controllerContext.GetControllerAttribute<DisableQueryAttribute>() ? .QueryParams ?? QueryParams.None;
 
-            foreach (var pair in query)
-            {
-                if (pair.Key.StartsWith("filter"))
-                {
+            foreach (var pair in query) {
+                if (pair.Key.StartsWith("filter")) {
                     if (disabledQueries.HasFlag(QueryParams.Filter) == false)
                         querySet.Filters.AddRange(ParseFilterQuery(pair.Key, pair.Value));
                     continue;
                 }
 
-                if (pair.Key.StartsWith("sort"))
-                {
+                if (pair.Key.StartsWith("sort")) {
                     if (disabledQueries.HasFlag(QueryParams.Sort) == false)
                         querySet.SortParameters = ParseSortParameters(pair.Value);
                     continue;
                 }
 
-                if (pair.Key.StartsWith("include"))
-                {
+                if (pair.Key.StartsWith("include")) {
                     if (disabledQueries.HasFlag(QueryParams.Include) == false)
                         querySet.IncludedRelationships = ParseIncludedRelationships(pair.Value);
                     continue;
                 }
 
-                if (pair.Key.StartsWith("page"))
-                {
+                if (pair.Key.StartsWith("page")) {
                     if (disabledQueries.HasFlag(QueryParams.Page) == false)
                         querySet.PageQuery = ParsePageQuery(querySet.PageQuery, pair.Key, pair.Value);
                     continue;
                 }
 
-                if (pair.Key.StartsWith("fields"))
-                {
+                if (pair.Key.StartsWith("fields")) {
                     if (disabledQueries.HasFlag(QueryParams.Fields) == false)
                         querySet.Fields = ParseFieldsQuery(pair.Key, pair.Value);
                     continue;
@@ -78,26 +67,24 @@ namespace JsonApiDotNetCore.Services
             return querySet;
         }
 
-        protected virtual List<FilterQuery> ParseFilterQuery(string key, string value)
-        {
+        protected virtual List<FilterQuery> ParseFilterQuery(string key, string value) {
             // expected input = filter[id]=1
             // expected input = filter[id]=eq:1
             var queries = new List<FilterQuery>();
 
-            var propertyName = key.Split('[', ']')[1].ToProperCase();
+            var propertyName = key.Split('[', ']') [1].ToProperCase();
 
             var values = value.Split(',');
-            foreach (var val in values)
-            {
-                (var operation, var filterValue) = ParseFilterOperation(val);
+            foreach (var val in values) {
+                (var operation,
+                    var filterValue) = ParseFilterOperation(val);
                 queries.Add(new FilterQuery(propertyName, filterValue, operation));
             }
 
             return queries;
         }
 
-        protected virtual (string operation, string value) ParseFilterOperation(string value)
-        {
+        protected virtual(string operation, string value) ParseFilterOperation(string value) {
             if (value.Length < 3)
                 return (string.Empty, value);
 
@@ -116,13 +103,12 @@ namespace JsonApiDotNetCore.Services
             return (prefix, value);
         }
 
-        protected virtual PageQuery ParsePageQuery(PageQuery pageQuery, string key, string value)
-        {
+        protected virtual PageQuery ParsePageQuery(PageQuery pageQuery, string key, string value) {
             // expected input = page[size]=10
             //                  page[number]=1
             pageQuery = pageQuery ?? new PageQuery();
 
-            var propertyName = key.Split('[', ']')[1];
+            var propertyName = key.Split('[', ']') [1];
 
             if (propertyName == "size")
                 pageQuery.PageSize = Convert.ToInt32(value);
@@ -134,28 +120,31 @@ namespace JsonApiDotNetCore.Services
 
         // sort=id,name
         // sort=-id
-        protected virtual List<SortQuery> ParseSortParameters(string value)
-        {
+        protected virtual List<SortQuery> ParseSortParameters(string value) {
+            const char SORT_DELIMITER = ',';
+            const char DESCENDING_SORT_OPERATOR = '-';
+
             var sortParameters = new List<SortQuery>();
-            value.Split(',').ToList().ForEach(p =>
-            {
+            var sortSegments = value.Split(SORT_DELIMITER);
+            foreach (var sortSegment in sortSegments) {
+
+                var propertyName = sortSegment;
                 var direction = SortDirection.Ascending;
-                if (p[0] == '-')
-                {
+
+                if (sortSegment[0] == DESCENDING_SORT_OPERATOR) {
                     direction = SortDirection.Descending;
-                    p = p.Substring(1);
+                    propertyName = propertyName.Substring(1);
                 }
 
-                var attribute = GetAttribute(p.ToProperCase());
+                var attribute = GetAttribute(propertyName);
 
                 sortParameters.Add(new SortQuery(direction, attribute));
-            });
+            };
 
             return sortParameters;
         }
 
-        protected virtual List<string> ParseIncludedRelationships(string value)
-        {
+        protected virtual List<string> ParseIncludedRelationships(string value) {
             if (value.Contains("."))
                 throw new JsonApiException(400, "Deeply nested relationships are not supported");
 
@@ -164,10 +153,9 @@ namespace JsonApiDotNetCore.Services
                 .ToList();
         }
 
-        protected virtual List<string> ParseFieldsQuery(string key, string value)
-        {
+        protected virtual List<string> ParseFieldsQuery(string key, string value) {
             // expected: fields[TYPE]=prop1,prop2
-            var typeName = key.Split('[', ']')[1];
+            var typeName = key.Split('[', ']') [1];
 
             var includedFields = new List<string> { "Id" };
 
@@ -175,8 +163,7 @@ namespace JsonApiDotNetCore.Services
                 return includedFields;
 
             var fields = value.Split(',');
-            foreach (var field in fields)
-            {
+            foreach (var field in fields) {
                 var internalAttrName = _controllerContext.RequestEntity
                     .Attributes
                     .SingleOrDefault(attr => attr.PublicAttributeName == field)
@@ -187,12 +174,11 @@ namespace JsonApiDotNetCore.Services
             return includedFields;
         }
 
-        protected virtual AttrAttribute GetAttribute(string propertyName)
-            => _controllerContext
-                .RequestEntity
-                .Attributes
-                .FirstOrDefault(attr =>
-                    string.Equals(attr.InternalAttributeName, propertyName, StringComparison.OrdinalIgnoreCase)
-                );
+        protected virtual AttrAttribute GetAttribute(string propertyName) => _controllerContext
+            .RequestEntity
+            .Attributes
+            .FirstOrDefault(attr =>
+                string.Equals(attr.PublicAttributeName, propertyName, StringComparison.OrdinalIgnoreCase)
+            );
     }
 }
