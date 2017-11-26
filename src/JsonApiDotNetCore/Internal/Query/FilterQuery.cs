1c96572043629e526cb2e99b44b31f0c35c9b96a
jaredcnance <jaredcnance@gmail.com>
2017-11-26 15:40:04 -0600

    feat(FilterQuery / QueryAccessor): use public attribute names for keys
    Closes #197

--- src/JsonApiDotNetCore/Internal/Query/FilterQuery.cs
+++ src/JsonApiDotNetCore/Internal/Query/FilterQuery.cs
@@ -1,17 +1,23 @@
+using System;
+using JsonApiDotNetCore.Extensions;
+
 namespace JsonApiDotNetCore.Internal.Query
 {
     public class FilterQuery
     {
-        public FilterQuery(string key, string value, string operation)
+        public FilterQuery(string attribute, string value, string operation)
         {
-            Key = key;
+            Attribute = attribute;
+            Key = attribute.ToProperCase();
             Value = value;
             Operation = operation;
         }
         
+        [Obsolete("Key has been replaced by '" + nameof(Attribute) + "'. Members should be located by their public name, not by coercing the provided value to the internal name.")]
         public string Key { get; set; }
+        public string Attribute { get; }
         public string Value { get; set; }
         public string Operation { get; set; }
-        public bool IsAttributeOfRelationship => Key.Contains(".");
+        public bool IsAttributeOfRelationship => Attribute.Contains(".");
     }
 }
