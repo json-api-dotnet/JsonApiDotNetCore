f2ae1127e303d0f3a335b81706ce192384c2fde6
jaredcnance <jaredcnance@gmail.com>
2017-12-19 06:43:50 -0600

    feat(AttrAttribute): add isSortable and isFilterable

---
+++ src/JsonApiDotNetCore/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleTo("UnitTests")]
+[assembly:InternalsVisibleTo("JsonApiDotNetCoreExampleTests")]
+[assembly:InternalsVisibleTo("NoEntityFrameworkTests")]
