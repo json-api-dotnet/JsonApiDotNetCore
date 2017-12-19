6b530b0c65445ba187340bd95c40af7596eda964
jaredcnance <jaredcnance@gmail.com>
2017-12-19 06:43:50 -0600

    feat(AttrAttribute): add isSortable and isFilterable

--- src/JsonApiDotNetCore/Models/AttrAttribute.cs
+++ src/JsonApiDotNetCore/Models/AttrAttribute.cs
@@ -1,27 +1,30 @@
 using System;
-using System.Reflection;
 using JsonApiDotNetCore.Internal;
 
 namespace JsonApiDotNetCore.Models
 {
     public class AttrAttribute : Attribute
     {
-        public AttrAttribute(string publicName, bool isImmutable = false)
+        public AttrAttribute(string publicName, bool isImmutable = false, bool isFilterable = true, bool isSortable = true)
         {
             PublicAttributeName = publicName;
             IsImmutable = isImmutable;
+            IsFilterable = isFilterable;
+            IsSortable = isSortable;
         }
 
-        public AttrAttribute(string publicName, string internalName, bool isImmutable = false)
+        internal AttrAttribute(string publicName, string internalName, bool isImmutable = false)
         {
             PublicAttributeName = publicName;
             InternalAttributeName = internalName;
             IsImmutable = isImmutable;
         }
 
-        public string PublicAttributeName { get; set; }
-        public string InternalAttributeName { get; set; }
-        public bool IsImmutable { get; set; }
+        public string PublicAttributeName { get; }
+        public string InternalAttributeName { get; }
+        public bool IsImmutable { get; }
+        public bool IsFilterable { get; }
+        public bool IsSortable { get; }
 
         public object GetValue(object entity)
         {
