3113aa57d100cd3fea25ba958a52a3268df2374e
Jared Nance <jaredcnance@gmail.com>
2017-12-19 12:52:15 -0600

    ci(Build.ps1): check exit codes

--- Build.ps1
+++ Build.ps1
@@ -9,34 +9,52 @@ function Get-Version-Suffix-From-Tag
   return $final
 }
 
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
+
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        $msg = "EXE RETURNED EXIT CODE $LastExitCode"
+        throw $msg
+    }
+}
+
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 dotnet restore
 
 dotnet test ./test/UnitTests/UnitTests.csproj
+CheckLastExitCode
+
 dotnet test ./test/JsonApiDotNetCoreExampleTests/JsonApiDotNetCoreExampleTests.csproj
+CheckLastExitCode
+
 dotnet test ./test/NoEntityFrameworkTests/NoEntityFrameworkTests.csproj
+CheckLastExitCode
 
 dotnet build .\src\JsonApiDotNetCore -c Release
+CheckLastExitCode
 
-echo "APPVEYOR_REPO_TAG: $env:APPVEYOR_REPO_TAG"
+Write-Output "APPVEYOR_REPO_TAG: $env:APPVEYOR_REPO_TAG"
 
 If($env:APPVEYOR_REPO_TAG -eq $true) {
     $revision = Get-Version-Suffix-From-Tag
-    echo "VERSION-SUFFIX: $revision"
+    Write-Output "VERSION-SUFFIX: $revision"
 
     IF ([string]::IsNullOrWhitespace($revision)){
-        echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts"
+        Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts"
         dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts
+        CheckLastExitCode
     }
     Else {
-        echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision"
+        Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision"
         dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision 
+        CheckLastExitCode
     }
 }
 Else { 
-    echo "VERSION-SUFFIX: alpha1-$revision"
-    echo "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision"
+    Write-Output "VERSION-SUFFIX: alpha1-$revision"
+    Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision"
     dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=alpha1-$revision 
+    CheckLastExitCode
 }
