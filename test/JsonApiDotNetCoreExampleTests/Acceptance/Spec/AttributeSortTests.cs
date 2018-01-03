072fa394ebe9d67d077e4256493e7b9d713c0c2c
jaredcnance <jaredcnance@gmail.com>
2018-01-03 06:00:41 -0600

    test(acceptance): verify filter/sort limitations

---
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/AttributeSortTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JsonApiDotNetCoreExample;
+using Xunit;
+
+namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
+{
+    [Collection("WebHostCollection")]
+    public class AttributeSortTests
+    {
+        private TestFixture<Startup> _fixture;
+
+        public AttributeSortTests(TestFixture<Startup> fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task Cannot_Sort_If_Explicitly_Forbidden()
+        {
+            // arrange
+            var httpMethod = new HttpMethod("GET");
+            var route = $"/api/v1/todo-items?include=owner&sort=achieved-date";
+            var request = new HttpRequestMessage(httpMethod, route);
+
+            // act
+            var response = await _fixture.Client.SendAsync(request);
+
+            // assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
