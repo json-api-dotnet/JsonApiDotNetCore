19e90b8fad1902ab763de451fcdb020006176cd1
jaredcnance <jaredcnance@gmail.com>
2018-02-17 18:16:43 -0600

    include stack traces when not in production

--- src/JsonApiDotNetCore/Internal/JsonApiExceptionFactory.cs
+++ src/JsonApiDotNetCore/Internal/JsonApiExceptionFactory.cs
@@ -1,36 +1,17 @@
 using System;
-using System.Linq;
 
 namespace JsonApiDotNetCore.Internal
 {
     public static class JsonApiExceptionFactory
     {
-        private const string JsonApiException = nameof(JsonApiException);
-        private const string InvalidCastException = nameof(InvalidCastException);
-
         public static JsonApiException GetException(Exception exception)
         {
-            var exceptionType = exception.GetType().ToString().Split('.').Last();
-            switch(exceptionType)
-            {
-                case JsonApiException:
-                    return (JsonApiException)exception;
-                case InvalidCastException:
-                    return new JsonApiException(409, exception.Message);
-                default:
-                    return new JsonApiException(500, exception.Message, GetExceptionDetail(exception.InnerException));
-            }
-        }
+            var exceptionType = exception.GetType();
 
-        private static string GetExceptionDetail(Exception exception)
-        {
-            string detail = null;
-            while(exception != null)
-            {
-                detail = $"{detail}{exception.Message}; ";
-                exception = exception.InnerException;
-            }
-            return detail;
+            if(exceptionType == typeof(JsonApiException))
+                return (JsonApiException)exception;
+                
+            return new JsonApiException(500, exceptionType.Name, exception);
         }
     }
 }
