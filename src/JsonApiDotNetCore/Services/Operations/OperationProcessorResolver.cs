1718a11b7b5859beaabbaef3bd329a3c4be56873
jaredcnance <jaredcnance@gmail.com>
2018-03-29 06:13:53 -0500

    fix(OperationProcessorResolver): do not cache opProcessors

--- src/JsonApiDotNetCore/Services/Operations/OperationProcessorResolver.cs
+++ src/JsonApiDotNetCore/Services/Operations/OperationProcessorResolver.cs
@@ -1,30 +1,42 @@
-using System.Collections.Concurrent;
 using JsonApiDotNetCore.Internal.Generics;
 using JsonApiDotNetCore.Models.Operations;
 using JsonApiDotNetCore.Services.Operations.Processors;
 
 namespace JsonApiDotNetCore.Services.Operations
 {
+    /// <summary>
+    /// Used to resolve <see cref="IOpProcessor"/> at runtime based on the required operation
+    /// </summary>
     public interface IOperationProcessorResolver
     {
+        /// <summary>
+        /// Locates the correct <see cref="CreateOpProcessor{T, TId}"/>
+        /// </summary>
         IOpProcessor LocateCreateService(Operation operation);
+
+        /// <summary>
+        /// Locates the correct <see cref="GetOpProcessor{T, TId}"/>
+        /// </summary>
         IOpProcessor LocateGetService(Operation operation);
+
+        /// <summary>
+        /// Locates the correct <see cref="RemoveOpProcessor{T, TId}"/>
+        /// </summary>
         IOpProcessor LocateRemoveService(Operation operation);
+
+        /// <summary>
+        /// Locates the correct <see cref="UpdateOpProcessor{T, TId}"/>
+        /// </summary>
         IOpProcessor LocateUpdateService(Operation operation);
     }
 
+    /// <inheritdoc />
     public class OperationProcessorResolver : IOperationProcessorResolver
     {
         private readonly IGenericProcessorFactory _processorFactory;
         private readonly IJsonApiContext _context;
 
-        // processor caches -- since there is some associated cost with creating the processors, we store them in memory
-        // to reduce the cost of subsequent requests. in the future, this may be moved into setup code run at startup
-        private ConcurrentDictionary<string, IOpProcessor> _createOpProcessors = new ConcurrentDictionary<string, IOpProcessor>();
-        private ConcurrentDictionary<string, IOpProcessor> _getOpProcessors = new ConcurrentDictionary<string, IOpProcessor>();
-        private ConcurrentDictionary<string, IOpProcessor> _removeOpProcessors = new ConcurrentDictionary<string, IOpProcessor>();
-        private ConcurrentDictionary<string, IOpProcessor> _updateOpProcessors = new ConcurrentDictionary<string, IOpProcessor>();
-
+        /// <nodoc />
         public OperationProcessorResolver(
             IGenericProcessorFactory processorFactory,
             IJsonApiContext context)
@@ -33,72 +45,54 @@ namespace JsonApiDotNetCore.Services.Operations
             _context = context;
         }
 
-        // TODO: there may be some optimizations here around the cache such as not caching processors
-        // if the request only contains a single op
+        /// <inheritdoc />
         public IOpProcessor LocateCreateService(Operation operation)
         {
             var resource = operation.GetResourceTypeName();
-
-            if (_createOpProcessors.TryGetValue(resource, out IOpProcessor cachedProcessor))
-                return cachedProcessor;
 
             var contextEntity = _context.ContextGraph.GetContextEntity(resource);
             var processor = _processorFactory.GetProcessor<IOpProcessor>(
                 typeof(ICreateOpProcessor<,>), contextEntity.EntityType, contextEntity.IdentityType
             );
 
-            _createOpProcessors[resource] = processor;
-
             return processor;
         }
 
+        /// <inheritdoc />
         public IOpProcessor LocateGetService(Operation operation)
         {
             var resource = operation.GetResourceTypeName();
-
-            if (_getOpProcessors.TryGetValue(resource, out IOpProcessor cachedProcessor))
-                return cachedProcessor;
 
             var contextEntity = _context.ContextGraph.GetContextEntity(resource);
             var processor = _processorFactory.GetProcessor<IOpProcessor>(
                 typeof(IGetOpProcessor<,>), contextEntity.EntityType, contextEntity.IdentityType
             );
 
-            _getOpProcessors[resource] = processor;
-
             return processor;
         }
 
+        /// <inheritdoc />
         public IOpProcessor LocateRemoveService(Operation operation)
         {
             var resource = operation.GetResourceTypeName();
-
-            if (_removeOpProcessors.TryGetValue(resource, out IOpProcessor cachedProcessor))
-                return cachedProcessor;
 
             var contextEntity = _context.ContextGraph.GetContextEntity(resource);
             var processor = _processorFactory.GetProcessor<IOpProcessor>(
                 typeof(IRemoveOpProcessor<,>), contextEntity.EntityType, contextEntity.IdentityType
             );
 
-            _removeOpProcessors[resource] = processor;
-
             return processor;
         }
 
+        /// <inheritdoc />
         public IOpProcessor LocateUpdateService(Operation operation)
         {
             var resource = operation.GetResourceTypeName();
-
-            if (_updateOpProcessors.TryGetValue(resource, out IOpProcessor cachedProcessor))
-                return cachedProcessor;
 
             var contextEntity = _context.ContextGraph.GetContextEntity(resource);
             var processor = _processorFactory.GetProcessor<IOpProcessor>(
                 typeof(IUpdateOpProcessor<,>), contextEntity.EntityType, contextEntity.IdentityType
             );
-
-            _updateOpProcessors[resource] = processor;
 
             return processor;
         }
