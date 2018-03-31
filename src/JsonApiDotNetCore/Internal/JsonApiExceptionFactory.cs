7f9015d297dfb4ed64fb6b40e64b353a10259266
jaredcnance <jaredcnance@gmail.com>
2018-03-31 11:12:20 -0500

    return 409 for mismatching data types

--- src/JsonApiDotNetCore/Internal/JsonApiExceptionFactory.cs
+++ src/JsonApiDotNetCore/Internal/JsonApiExceptionFactory.cs
@@ -8,9 +8,17 @@ namespace JsonApiDotNetCore.Internal
         {
             var exceptionType = exception.GetType();
 
-            if(exceptionType == typeof(JsonApiException))
+            if (exceptionType == typeof(JsonApiException))
                 return (JsonApiException)exception;
-                
+
+            // TODO: this is for mismatching type requests (e.g. posting an author to articles endpoint)
+            // however, we can't actually guarantee that this is the source of this exception
+            // we should probably use an action filter or when we improve the ContextGraph 
+            // we might be able to skip most of deserialization entirely by checking the JToken
+            // directly
+            if (exceptionType == typeof(InvalidCastException))
+                return new JsonApiException(409, exception.Message, exception);
+
             return new JsonApiException(500, exceptionType.Name, exception);
         }
     }
