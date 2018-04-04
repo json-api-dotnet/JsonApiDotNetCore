961383564aed289de335af5ce7fe9ce77d283196
jaredcnance <jaredcnance@gmail.com>
2018-04-04 15:50:21 -0500

    fix(ContextGraphBuilder): don't throw if DbContext contains non json:api resource
    Also begins work for #170

--- src/JsonApiDotNetCore/Extensions/IApplicationBuilderExtensions.cs
+++ src/JsonApiDotNetCore/Extensions/IApplicationBuilderExtensions.cs
@@ -1,7 +1,10 @@
+using JsonApiDotNetCore.Builders;
 using JsonApiDotNetCore.Configuration;
+using JsonApiDotNetCore.Internal;
 using JsonApiDotNetCore.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace JsonApiDotNetCore.Extensions
 {
@@ -10,13 +13,8 @@ namespace JsonApiDotNetCore.Extensions
     {
         public static IApplicationBuilder UseJsonApi(this IApplicationBuilder app, bool useMvc = true)
         {
-            var environment = (IHostingEnvironment)app.ApplicationServices.GetService(typeof(IHostingEnvironment));
-
-            if(environment.IsProduction())
-            {
-                JsonApiOptions.DisableErrorStackTraces = true;
-                JsonApiOptions.DisableErrorSource = true;
-            }
+            DisableDetailedErrorsIfProduction(app);
+            LogContextGraphValidations(app);
 
             app.UseMiddleware<RequestMiddleware>();
 
@@ -24,6 +22,34 @@ namespace JsonApiDotNetCore.Extensions
                 app.UseMvc();
 
             return app;
+        }
+
+        private static void DisableDetailedErrorsIfProduction(IApplicationBuilder app)
+        {
+            var environment = (IHostingEnvironment)app.ApplicationServices.GetService(typeof(IHostingEnvironment));
+
+            if (environment.IsProduction())
+            {
+                JsonApiOptions.DisableErrorStackTraces = true;
+                JsonApiOptions.DisableErrorSource = true;
+            }
+        }
+
+        private static void LogContextGraphValidations(IApplicationBuilder app)
+        {
+            var logger = app.ApplicationServices.GetService(typeof(ILogger<ContextGraphBuilder>)) as ILogger;
+            var contextGraph = app.ApplicationServices.GetService(typeof(IContextGraph)) as ContextGraph;
+
+            if (logger != null && contextGraph != null)
+            {
+                contextGraph.ValidationResults.ForEach((v) =>
+                    logger.Log(
+                        v.LogLevel,
+                        new EventId(),
+                        v.Message,
+                        exception: null,
+                        formatter: (m, e) => m));
+            }
         }
     }
 }
