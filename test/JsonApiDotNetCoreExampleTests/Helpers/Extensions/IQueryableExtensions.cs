102f6f28d5aad6122c1a463351192ae271064625
jaredcnance <jaredcnance@gmail.com>
2018-06-13 14:32:05 -0500

    handle internal changes across multiple versions of EF Core

--- test/JsonApiDotNetCoreExampleTests/Helpers/Extensions/IQueryableExtensions.cs
+++ test/JsonApiDotNetCoreExampleTests/Helpers/Extensions/IQueryableExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -11,6 +12,19 @@ using Database = Microsoft.EntityFrameworkCore.Storage.Database;
 namespace JsonApiDotNetCoreExampleTests.Helpers.Extensions
 {
     public static class IQueryableExtensions
+    {
+        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        {
+            var efCoreVersion = FileVersionInfo.GetVersionInfo(typeof(QueryCompiler).Assembly.Location);
+            var majorMinor = $"{efCoreVersion.ProductMajorPart}.{efCoreVersion.ProductMinorPart}";
+
+            return majorMinor == "2.1"
+                ? QueryGenerator_2_1_0.ToSql(query)
+                : QueryGenerator_2_0_0.ToSql(query);
+        }
+    }
+
+    public static class QueryGenerator_2_0_0
     {
         private static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
 
@@ -25,7 +39,7 @@ namespace JsonApiDotNetCoreExampleTests.Helpers.Extensions
         private static readonly PropertyInfo DatabaseDependenciesField
             = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
 
-        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        public static string ToSql<TEntity>(IQueryable<TEntity> query) where TEntity : class
         {
             if (!(query is EntityQueryable<TEntity>) && !(query is InternalDbSet<TEntity>))
             {
@@ -44,6 +58,36 @@ namespace JsonApiDotNetCoreExampleTests.Helpers.Extensions
             var sql = modelVisitor.Queries.First().ToString();
 
             return sql;
+        }
+    }
+
+    public static class QueryGenerator_2_1_0
+    {
+        private static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.Single(x => x.Name == "_queryCompiler");
+
+        private static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
+
+        private static readonly FieldInfo QueryModelGeneratorField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_queryModelGenerator");
+
+        private static readonly FieldInfo DatabaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
+
+        private static readonly PropertyInfo DependenciesProperty = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
+
+        public static string ToSql<TEntity>(IQueryable<TEntity> queryable)
+            where TEntity : class
+        {
+            if (!(queryable is EntityQueryable<TEntity>) && !(queryable is InternalDbSet<TEntity>))
+                throw new ArgumentException();
+
+            var queryCompiler = (IQueryCompiler)QueryCompilerField.GetValue(queryable.Provider);
+            var queryModelGenerator = (IQueryModelGenerator)QueryModelGeneratorField.GetValue(queryCompiler);
+            var queryModel = queryModelGenerator.ParseQuery(queryable.Expression);
+            var database = DatabaseField.GetValue(queryCompiler);
+            var queryCompilationContextFactory = ((DatabaseDependencies)DependenciesProperty.GetValue(database)).QueryCompilationContextFactory;
+            var queryCompilationContext = queryCompilationContextFactory.Create(false);
+            var modelVisitor = (RelationalQueryModelVisitor)queryCompilationContext.CreateQueryModelVisitor();
+            modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
+            return modelVisitor.Queries.Join(Environment.NewLine + Environment.NewLine);
         }
     }
 }
