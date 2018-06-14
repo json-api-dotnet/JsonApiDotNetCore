7280ff7f14c5045ed80f9e4fc59cead7be11125b
jaredcnance <jaredcnance@gmail.com>
2018-06-13 20:30:49 -0500

    fix test issue and bump package version to 2.3.0

--- test/JsonApiDotNetCoreExampleTests/Helpers/Extensions/IQueryableExtensions.cs
+++ test/JsonApiDotNetCoreExampleTests/Helpers/Extensions/IQueryableExtensions.cs
@@ -12,38 +12,31 @@ namespace JsonApiDotNetCoreExampleTests.Helpers.Extensions
 {
     public static class IQueryableExtensions
     {
+        private static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.Single(x => x.Name == "_queryCompiler");
+
         private static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
 
-        private static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryCompiler");
+        private static readonly FieldInfo QueryModelGeneratorField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_queryModelGenerator");
 
-        private static readonly PropertyInfo NodeTypeProviderField = QueryCompilerTypeInfo.DeclaredProperties.Single(x => x.Name == "NodeTypeProvider");
+        private static readonly FieldInfo DatabaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
 
-        private static readonly MethodInfo CreateQueryParserMethod = QueryCompilerTypeInfo.DeclaredMethods.First(x => x.Name == "CreateQueryParser");
+        private static readonly PropertyInfo DependenciesProperty = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
 
-        private static readonly FieldInfo DataBaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
-
-        private static readonly PropertyInfo DatabaseDependenciesField
-            = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
-
-        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        public static string ToSql<TEntity>(this IQueryable<TEntity> queryable)
+            where TEntity : class
         {
-            if (!(query is EntityQueryable<TEntity>) && !(query is InternalDbSet<TEntity>))
-            {
-                throw new ArgumentException("Invalid query");
-            }
+            if (!(queryable is EntityQueryable<TEntity>) && !(queryable is InternalDbSet<TEntity>))
+                throw new ArgumentException();
 
-            var queryCompiler = (IQueryCompiler)QueryCompilerField.GetValue(query.Provider);
-            var nodeTypeProvider = (INodeTypeProvider)NodeTypeProviderField.GetValue(queryCompiler);
-            var parser = (IQueryParser)CreateQueryParserMethod.Invoke(queryCompiler, new object[] { nodeTypeProvider });
-            var queryModel = parser.GetParsedQuery(query.Expression);
-            var database = DataBaseField.GetValue(queryCompiler);
-            var queryCompilationContextFactory = ((DatabaseDependencies)DatabaseDependenciesField.GetValue(database)).QueryCompilationContextFactory;
+            var queryCompiler = (IQueryCompiler)QueryCompilerField.GetValue(queryable.Provider);
+            var queryModelGenerator = (IQueryModelGenerator)QueryModelGeneratorField.GetValue(queryCompiler);
+            var queryModel = queryModelGenerator.ParseQuery(queryable.Expression);
+            var database = DatabaseField.GetValue(queryCompiler);
+            var queryCompilationContextFactory = ((DatabaseDependencies)DependenciesProperty.GetValue(database)).QueryCompilationContextFactory;
             var queryCompilationContext = queryCompilationContextFactory.Create(false);
             var modelVisitor = (RelationalQueryModelVisitor)queryCompilationContext.CreateQueryModelVisitor();
             modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
-            var sql = modelVisitor.Queries.First().ToString();
-
-            return sql;
+            return modelVisitor.Queries.Join(Environment.NewLine + Environment.NewLine);
         }
     }
 }
