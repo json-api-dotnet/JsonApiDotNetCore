5a07e55a2bb21c6b6f19f642ec91b937cb52d4da
Nathan Wise <nathanwise@gmail.com>
2018-06-15 15:36:15 -0400

    Fixing issue where total-records was 0 on POST and PATCH calls.

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/DocumentTests/Meta.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/DocumentTests/Meta.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using JsonApiDotNetCore.Models;
 using JsonApiDotNetCoreExample;
@@ -47,6 +48,119 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec.DocumentTests
             var response = await client.SendAsync(request);
             var responseBody = await response.Content.ReadAsStringAsync();
             var documents = JsonConvert.DeserializeObject<Documents>(responseBody);
+            
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(documents.Meta);
+            Assert.Equal((long)expectedCount, (long)documents.Meta["total-records"]);
+        }
+
+        [Fact]
+        public async Task Total_Record_Count_Not_Included_When_None()
+        {
+            // arrange
+            _context.TodoItems.RemoveRange(_context.TodoItems);
+            _context.SaveChanges();
+            var builder = new WebHostBuilder()
+                .UseStartup<MetaStartup>();
+
+            var httpMethod = new HttpMethod("GET");
+            var route = $"/api/v1/todo-items";
+
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+            var request = new HttpRequestMessage(httpMethod, route);
+
+            // act
+            var response = await client.SendAsync(request);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var documents = JsonConvert.DeserializeObject<Documents>(responseBody);
+            
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Null(documents.Meta);
+        }
+
+        [Fact]
+        public async Task Total_Record_Count_Included_POST()
+        {
+            // arrange
+            _context.TodoItems.RemoveRange(_context.TodoItems);
+            _context.SaveChanges();
+            var expectedCount = 1;
+            var builder = new WebHostBuilder()
+                .UseStartup<MetaStartup>();
+
+            var httpMethod = new HttpMethod("POST");
+            var route = $"/api/v1/todo-items";
+
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+            var request = new HttpRequestMessage(httpMethod, route);
+            var content = new
+            {
+                data = new
+                {
+                    type = "todo-items",
+                    attributes = new
+                    {
+                        description = "New Description",
+                    }
+                }
+            };
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(content));
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.api+json");
+
+            // act
+            var response = await client.SendAsync(request);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var documents = JsonConvert.DeserializeObject<Document>(responseBody);
+            
+            // assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.NotNull(documents.Meta);
+            Assert.Equal((long)expectedCount, (long)documents.Meta["total-records"]);
+        }
+
+        [Fact]
+        public async Task Total_Record_Count_Included_PATCH()
+        {
+            // arrange
+            _context.TodoItems.RemoveRange(_context.TodoItems);
+            TodoItem todoItem = new TodoItem();
+            _context.TodoItems.Add(todoItem);
+            _context.SaveChanges();
+            var expectedCount = 1;
+            var builder = new WebHostBuilder()
+                .UseStartup<MetaStartup>();
+
+            var httpMethod = new HttpMethod("PATCH");
+            var route = $"/api/v1/todo-items/{todoItem.Id}";
+
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+            var request = new HttpRequestMessage(httpMethod, route);
+            var content = new
+            {
+                data = new
+                {
+                    type = "todo-items",
+                    id = todoItem.Id,
+                    attributes = new
+                    {
+                        description = "New Description",
+                    }
+                }
+            };
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(content));
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.api+json");
+
+            // act
+            var response = await client.SendAsync(request);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var documents = JsonConvert.DeserializeObject<Document>(responseBody);
             
             // assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
