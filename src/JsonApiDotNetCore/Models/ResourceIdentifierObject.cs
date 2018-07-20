8d6ef80bbe0f61d9e293a62329df850e13266815
jaredcnance <jaredcnance@gmail.com>
2018-07-19 22:14:19 -0700

    test(Deserializer): add test for independent id setting

--- src/JsonApiDotNetCore/Models/ResourceIdentifierObject.cs
+++ src/JsonApiDotNetCore/Models/ResourceIdentifierObject.cs
@@ -4,9 +4,16 @@ namespace JsonApiDotNetCore.Models
 {
     public class ResourceIdentifierObject
     {
+        public ResourceIdentifierObject() { }
+        public ResourceIdentifierObject(string type, string id)
+        {
+            Type = type;
+            Id = id;
+        }
+
         [JsonProperty("type")]
         public string Type { get; set; }
-        
+
         [JsonProperty("id")]
         public string Id { get; set; }
 
