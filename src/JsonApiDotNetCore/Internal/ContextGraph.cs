2cbf4b8b9a1878ef2e15f5c81a814f72bfdec4a5
jaredcnance <jaredcnance@gmail.com>
2018-07-25 21:03:30 -0700

    doc(ContextGraph): add API documentation

--- src/JsonApiDotNetCore/Internal/ContextGraph.cs
+++ src/JsonApiDotNetCore/Internal/ContextGraph.cs
@@ -6,10 +6,45 @@ namespace JsonApiDotNetCore.Internal
 {
     public interface IContextGraph
     {
-        object GetRelationship<TParent>(TParent entity, string relationshipName);
+        /// <summary>
+        /// Gets the value of the navigation property, defined by the relationshipName, 
+        /// on the provided instance.
+        /// </summary>
+        /// <param name="resource">The resource instance</param>
+        /// <param name="propertyName">The navigation property name.</param>
+        /// <example>
+        /// <code>
+        /// _graph.GetRelationship(todoItem, nameof(TodoItem.Owner));
+        /// </code>
+        /// </example>
+        object GetRelationship<TParent>(TParent resource, string propertyName);
+
+        /// <summary>
+        /// Get the internal navigation property name for the specified public
+        /// relationship name.
+        /// </summary>
+        /// <param name="relationshipName">The public relationship name specified by a <see cref="HasOneAttribute" /> or <see cref="HasManyAttribute" /></param>
+        /// <example>
+        /// <code>
+        /// _graph.GetRelationshipName&lt;TodoItem&gt;("achieved-date");
+        /// // returns "AchievedDate"
+        /// </code>
+        /// </example>
         string GetRelationshipName<TParent>(string relationshipName);
+
+        /// <summary>
+        /// Get the resource metadata by the DbSet property name
+        /// </summary>
         ContextEntity GetContextEntity(string dbSetName);
+
+        /// <summary>
+        /// Get the resource metadata by the resource type
+        /// </summary>
         ContextEntity GetContextEntity(Type entityType);
+
+        /// <summary>
+        /// Was built against an EntityFrameworkCore DbContext ?
+        /// </summary>
         bool UsesDbContext { get; }
     }
 
@@ -40,14 +75,18 @@ namespace JsonApiDotNetCore.Internal
             Instance = this;
         }
 
+        /// </ inheritdoc>
         public bool UsesDbContext { get; }
 
+        /// </ inheritdoc>
         public ContextEntity GetContextEntity(string entityName)
             => Entities.SingleOrDefault(e => string.Equals(e.EntityName, entityName, StringComparison.OrdinalIgnoreCase));
 
+        /// </ inheritdoc>
         public ContextEntity GetContextEntity(Type entityType)
             => Entities.SingleOrDefault(e => e.EntityType == entityType);
 
+        /// </ inheritdoc>
         public object GetRelationship<TParent>(TParent entity, string relationshipName)
         {
             var parentEntityType = entity.GetType();
@@ -62,6 +101,7 @@ namespace JsonApiDotNetCore.Internal
             return navigationProperty.GetValue(entity);
         }
 
+        /// </ inheritdoc>
         public string GetRelationshipName<TParent>(string relationshipName)
         {
             var entityType = typeof(TParent);
