7e22eebc583d33c4ba7b4a91a493619bd9bc47ec
Ryan Tablada <ryan.tablada@gmail.com>
2018-08-07 23:55:08 -0500

    feat: obsolete method ConvertToErrorCollection

--- src/JsonApiDotNetCore/Extensions/ModelStateExtensions.cs
+++ src/JsonApiDotNetCore/Extensions/ModelStateExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using JsonApiDotNetCore.Internal;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore.Internal;
@@ -6,6 +7,26 @@ namespace JsonApiDotNetCore.Extensions
 {
     public static class ModelStateExtensions
     {
+        [Obsolete("Use Generic Method ConvertToErrorCollection<T>(IContextGraph contextGraph) instead for full validation errors")]
+        public static ErrorCollection ConvertToErrorCollection(this ModelStateDictionary modelState)
+        {
+            ErrorCollection collection = new ErrorCollection();
+            foreach (var entry in modelState)
+            {
+                if (entry.Value.Errors.Any() == false)
+                    continue;
+
+                foreach (var modelError in entry.Value.Errors)
+                {
+                    if (modelError.Exception is JsonApiException jex)
+                        collection.Errors.AddRange(jex.GetError().Errors);
+                    else
+                        collection.Errors.Add(new Error(400, entry.Key, modelError.ErrorMessage, modelError.Exception != null ? ErrorMeta.FromException(modelError.Exception) : null));
+                }
+            }
+
+            return collection;
+        }
         public static ErrorCollection ConvertToErrorCollection<T>(this ModelStateDictionary modelState, IContextGraph contextGraph)
         {
             ErrorCollection collection = new ErrorCollection();
