1506270b3a46752f45a9491c5ef8b8e063d69e21
Ryan Tablada <ryan.tablada@gmail.com>
2018-08-28 16:11:52 -0700

    feat: error status code should be first in collection

---
+++ test/UnitTests/Internal/JsonApiException_Test.cs
@@ -0,0 +1,31 @@
+using JsonApiDotNetCore.Internal;
+using Xunit;
+
+namespace UnitTests.Internal
+{
+    public class JsonApiException_Test
+    {
+        [Fact]
+        public void Can_GetStatusCode()
+        {
+            var errors = new ErrorCollection();
+            var exception = new JsonApiException(errors);
+
+            // Add First 422 error
+            errors.Add(new Error(422, "Something wrong"));
+            Assert.Equal(422, exception.GetStatusCode());
+
+            // Add a second 422 error
+            errors.Add(new Error(422, "Something else wrong"));
+            Assert.Equal(422, exception.GetStatusCode());
+
+            // Add 4xx error not 422
+            errors.Add(new Error(401, "Unauthorized"));
+            Assert.Equal(400, exception.GetStatusCode());
+
+            // Add 5xx error not 4xx
+            errors.Add(new Error(502, "Not good"));
+            Assert.Equal(500, exception.GetStatusCode());
+        }
+    }
+}
