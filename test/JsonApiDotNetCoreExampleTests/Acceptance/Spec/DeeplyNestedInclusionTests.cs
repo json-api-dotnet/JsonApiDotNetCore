c7887b8d98ddc096d7a2519d446bacc51bc21625
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-29 13:41:19 -0400

    try to assert some more complicated stuff, and check specific included records

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/DeeplyNestedInclusionTests.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/DeeplyNestedInclusionTests.cs
@@ -24,6 +24,14 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             _fixture = fixture;
         }
 
+        private void ResetContext(AppDbContext context) 
+        {
+            context.TodoItems.RemoveRange(context.TodoItems);
+            context.TodoItemCollections.RemoveRange(context.TodoItemCollections);
+            context.People.RemoveRange(context.People);
+            context.PersonRoles.RemoveRange(context.PersonRoles);
+        }
+
         [Fact]
         public async Task Can_Include_Nested_Relationships()
         {
@@ -72,8 +80,10 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
                 }
             };
         
+            
             var context = _fixture.GetService<AppDbContext>();
-            context.TodoItems.RemoveRange(context.TodoItems);
+            ResetContext(context);
+
             context.TodoItems.Add(todoItem);
             await context.SaveChangesAsync();
 
@@ -109,7 +119,8 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             };
         
             var context = _fixture.GetService<AppDbContext>();
-            context.TodoItems.RemoveRange(context.TodoItems);
+            ResetContext(context);
+
             context.TodoItems.Add(todoItem);
             await context.SaveChangesAsync();
 
@@ -147,7 +158,8 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             };
         
             var context = _fixture.GetService<AppDbContext>();
-            context.TodoItems.RemoveRange(context.TodoItems);
+            ResetContext(context);
+
             context.TodoItems.Add(todoItem);
             await context.SaveChangesAsync();
 
@@ -163,5 +175,59 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             
             Assert.Equal(7, included.Count); // 1 collection, 3 todos, 2 owners, 1 role
         }
+
+        [Fact]
+        public async Task Included_Resources_Are_Correct()
+        {
+            // arrange
+            var role = new PersonRole();
+            var asignee = new Person { Role = role };
+            var collectionOwner = new Person();
+            var someOtherOwner = new Person();
+            var collection = new TodoItemCollection { Owner = collectionOwner };
+            var todoItem1 = new TodoItem { Collection = collection, Assignee = asignee };
+            var todoItem2 = new TodoItem { Collection = collection, Assignee = asignee };
+            var todoItem3 = new TodoItem { Collection = collection, Owner = someOtherOwner };
+            var todoItem4 = new TodoItem { Collection = collection, Owner = asignee };
+
+
+            string route = 
+                "/api/v1/todo-items/" + todoItem1.Id + "?include=" +
+                    "collection.owner," + 
+                    "asignee.role," + 
+                    "asignee.assigned-todo-items";
+
+        
+            var context = _fixture.GetService<AppDbContext>();
+            ResetContext(context);
+
+            context.TodoItems.Add(todoItem1);
+            context.TodoItems.Add(todoItem2);
+            context.TodoItems.Add(todoItem3);
+            context.TodoItems.Add(todoItem4);
+            context.PersonRoles.Add(role);
+            context.People.Add(asignee);
+            context.People.Add(collectionOwner);
+            context.People.Add(someOtherOwner);
+            context.TodoItemCollections.Add(collection);
+
+
+            await context.SaveChangesAsync();
+
+            // act
+            var response = await _fixture.Client.GetAsync(route);
+
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var documents = JsonConvert.DeserializeObject<Document>(body);
+            var included = documents.Included;
+            
+            // 1 collection, 1 owner, 
+            // 1 asignee, 1 asignee role,
+            // 2 assigned todo items
+            Assert.Equal(6, included.Count); 
+        }        
     }
 }
