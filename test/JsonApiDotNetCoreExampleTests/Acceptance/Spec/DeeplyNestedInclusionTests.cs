bea642d45183268873571f70cd7eef3758fba6ca
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-29 11:40:45 -0400

    work on adding tests for different scenarios

--- test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/DeeplyNestedInclusionTests.cs
+++ test/JsonApiDotNetCoreExampleTests/Acceptance/Spec/DeeplyNestedInclusionTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -52,6 +53,86 @@ namespace JsonApiDotNetCoreExampleTests.Acceptance.Spec
             Assert.NotNull(responseTodoItem);
             Assert.NotNull(responseTodoItem.Collection);
             Assert.NotNull(responseTodoItem.Collection.Owner);
+        }
+
+        [Fact]
+        public async Task Can_Include_Nested_HasMany_Relationships()
+        {
+            // arrange
+            const string route = "/api/v1/todo-items?include=collection.todo-items";
+
+            var todoItem = new TodoItem {
+                Collection = new TodoItemCollection {
+                    Owner = new Person(),
+                    TodoItems = new List<TodoItem> {
+                        new TodoItem(),
+                        new TodoItem()
+                    }
+                }
+            };
+        
+            var context = _fixture.GetService<AppDbContext>();
+            context.TodoItems.RemoveRange(context.TodoItems);
+            context.TodoItems.Add(todoItem);
+            await context.SaveChangesAsync();
+
+            // act
+            var response = await _fixture.Client.GetAsync(route);
+
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var todoItems = _fixture.DeSerializer.DeserializeList<TodoItem>(body);
+
+            var responseTodoItem = todoItems[0];
+            Assert.NotNull(responseTodoItem);
+            Assert.NotNull(responseTodoItem.Collection);
+            Assert.NotNull(responseTodoItem.Collection.TodoItems);
+            Assert.Equal(2, responseTodoItem.Collection.TodoItems.Count);
+
+            // TODO: assert number of things in included
+        }
+
+        [Fact]
+        public async Task Can_Include_Nested_HasMany_Relationships_BelongsTo()
+        {
+            // arrange
+            const string route = "/api/v1/todo-items?include=collection.todo-items.owner";
+
+            var todoItem = new TodoItem {
+                Collection = new TodoItemCollection {
+                    Owner = new Person(),
+                    TodoItems = new List<TodoItem> {
+                        new TodoItem {
+                            Owner = new Person()
+                        },
+                        new TodoItem()
+                    }
+                }
+            };
+        
+            var context = _fixture.GetService<AppDbContext>();
+            context.TodoItems.RemoveRange(context.TodoItems);
+            context.TodoItems.Add(todoItem);
+            await context.SaveChangesAsync();
+
+            // act
+            var response = await _fixture.Client.GetAsync(route);
+
+            // assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var todoItems = _fixture.DeSerializer.DeserializeList<TodoItem>(body);
+
+            var responseTodoItem = todoItems[0];
+            Assert.NotNull(responseTodoItem);
+            Assert.NotNull(responseTodoItem.Collection);
+            Assert.NotNull(responseTodoItem.Collection.TodoItems);
+            Assert.Equal(2, responseTodoItem.Collection.TodoItems.Count);
+
+            // TODO: assert number of things in included
         }
     }
 }
