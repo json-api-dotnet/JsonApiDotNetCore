84c27b77fc7d9db4a46924aa23e5097eb31d9dd0
Maurits Moeys <maurits.moeys@gmail.com>
2019-05-14 14:26:41 +0200

    comment: InverseRelationshipResolver

--- src/JsonApiDotNetCore/Internal/InverseRelationships.cs
+++ src/JsonApiDotNetCore/Internal/InverseRelationships.cs
@@ -6,6 +6,16 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace JsonApiDotNetCore.Internal
 {
+    /// <summary>
+    /// Responsible for populating the RelationshipAttribute InverseNavigation property.
+    /// 
+    /// This service is instantiated in the configure phase of the application.
+    /// 
+    /// When using a data access layer different from EF Core, and when using ResourceHooks
+    /// that depend on the inverse navigation property (BeforeImplicitUpdateRelationship),
+    /// you will need to override this service, or pass along the inverseNavigationProperty in
+    /// the RelationshipAttribute.
+    /// </summary>
     public interface IInverseRelationships
     {
         void Resolve();
