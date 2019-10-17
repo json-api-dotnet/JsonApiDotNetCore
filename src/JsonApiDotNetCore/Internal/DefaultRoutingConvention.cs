db683044676d6c1a3f1e8ffb96b26ccde17ef7bd
Maurits Moeys <maurits.moeys@gmail.com>
2019-10-17 16:18:12 +0200

    docs: comments improved

--- src/JsonApiDotNetCore/Internal/DefaultRoutingConvention.cs
+++ src/JsonApiDotNetCore/Internal/DefaultRoutingConvention.cs
@@ -14,10 +14,11 @@ namespace JsonApiDotNetCore.Internal
     /// <summary>
     /// The default routing convention registers the name of the resource as the route
     /// using the <see cref="IResourceNameFormatter"/> that is registered. The default for this is
-    /// a kebab-case formatter.
+    /// a kebab-case formatter. If the controller is directly inherits from JsonApiMixin and there is no
+    /// resource directly associated, it used the name of the controller instead of the name of the type.
     /// </summary>
     /// <example>
-    /// public class SomeResourceController{SomeResource} : JsonApiController{SomeResource} { }
+    /// public class SomeResourceController: JsonApiController{SomeResource} { }
     /// // => /some-resources/relationship/related-resource
     ///
     /// public class RandomNameController{SomeResource} : JsonApiController{SomeResource} { }
@@ -26,6 +27,10 @@ namespace JsonApiDotNetCore.Internal
     /// // when using the camelCase formatter:
     /// public class SomeResourceController{SomeResource} : JsonApiController{SomeResource} { }
     /// // => /someResources/relationship/relatedResource
+    ///
+    /// // when inheriting from JsonApiMixin formatter:
+    /// public class SomeVeryCustomController{SomeResource} : JsonApiMixin { }
+    /// // => /some-very-customs/relationship/related-resource
     /// </example>
     public class DefaultRoutingConvention : IJsonApiRoutingConvention
     {
@@ -38,6 +43,7 @@ namespace JsonApiDotNetCore.Internal
             _formatter = formatter;
         }
 
+        /// <inheritdoc/>
         public void Apply(ApplicationModel application)
         {
             foreach (var controller in application.Controllers)
@@ -53,6 +59,9 @@ namespace JsonApiDotNetCore.Internal
             }
         }
 
+        /// <summary>
+        /// verifies if routing convention should be enabled for this controller
+        /// </summary>
         private bool RoutingConventionDisabled(ControllerModel controller)
         {
             var type = controller.ControllerType;
@@ -60,6 +69,9 @@ namespace JsonApiDotNetCore.Internal
             return notDisabled && type.IsSubclassOf(typeof(JsonApiControllerMixin));
         }
 
+        /// <summary>
+        /// Derives a template from the resource type, and checks if this template was already registered.
+        /// </summary>
         private string TemplateFromResource(ControllerModel model)
         {
             var resourceType = GetResourceTypeFromController(model.ControllerType);
@@ -72,6 +84,9 @@ namespace JsonApiDotNetCore.Internal
             return null;
         }
 
+        /// <summary>
+        /// Derives a template from the controller name, and checks if this template was already registered.
+        /// </summary>
         private string TemplateFromController(ControllerModel model)
         {
             var template = $"{_namespace}/{_formatter.ApplyCasingConvention(model.ControllerName)}";
@@ -80,6 +95,9 @@ namespace JsonApiDotNetCore.Internal
             return null;
         }
 
+        /// <summary>
+        /// Determines the resource associated to a controller by inspecting generic arguments.
+        /// </summary>
         private Type GetResourceTypeFromController(Type type)
         {
             var target = typeof(BaseJsonApiController<,>);
--- src/JsonApiDotNetCore/Internal/IJsonApiRoutingConvention.cs
+++ src/JsonApiDotNetCore/Internal/IJsonApiRoutingConvention.cs
@@ -1,10 +1,10 @@
-﻿// REF: https://github.com/aspnet/Entropy/blob/dev/samples/Mvc.CustomRoutingConvention/NameSpaceRoutingConvention.cs
-// REF: https://github.com/aspnet/Mvc/issues/5691
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace JsonApiDotNetCore.Internal
 {
-    public interface IJsonApiRoutingConvention : IApplicationModelConvention
-    {
-    }
+    /// <summary>
+    /// Service for specifying which routing convention to use. This can be overriden to customize
+    /// the relation between controllers and mapped routes.
+    /// </summary>
+    public interface IJsonApiRoutingConvention : IApplicationModelConvention { }
 }
