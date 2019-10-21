95d870755c3cdbf588501d909ce68246d753d3dc
Maurits Moeys <maurits.moeys@gmail.com>
2019-10-21 19:31:44 +0200

    chore: various fixes for PR review, mostly style and variable naming

---
+++ src/JsonApiDotNetCore/Internal/IDefaultRoutingConvention.cs
@@ -0,0 +1,11 @@
+﻿// REF: https://github.com/aspnet/Entropy/blob/dev/samples/Mvc.CustomRoutingConvention/NameSpaceRoutingConvention.cs
+// REF: https://github.com/aspnet/Mvc/issues/5691
+using System;
+
+namespace JsonApiDotNetCore.Internal
+{
+    public interface IControllerResourceMapping
+    {
+        Type GetAssociatedResource(string controllerName);
+    }
+}
