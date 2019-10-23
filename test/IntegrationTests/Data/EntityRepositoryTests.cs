80ea1416fe6e7a06f146cc50c4569054453a6222
Harro van der Kroft <wisepotato@users.noreply.github.com>
2019-10-23 16:55:44 +0200

    chore: start of getRepostory

--- test/IntegrationTests/Data/EntityRepositoryTests.cs
+++ test/IntegrationTests/Data/EntityRepositoryTests.cs
@@ -1,14 +1,23 @@
+using JsonApiDotNetCore.Builders;
+using JsonApiDotNetCore.Data;
+using JsonApiDotNetCoreExample.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace IntegrationTests.Data
+namespace JADNC.IntegrationTests.Data
 {
     public class EntityRepositoryTests
     {
 
+
+        public EntityRepositoryTests()
+        {
+            // setup database + services
+            // seed
+        }
         [Theory]
         [InlineData(6, -1, new[] { 4, 5, 6, 7, 8, 9 })]
         [InlineData(6, -2, new[] { 1, 2, 3 })]
@@ -24,6 +33,28 @@ namespace IntegrationTests.Data
 
             // Assert
             Assert.Equal(TodoItems(expectedIds), result, new IdComparer<TodoItem>());
+        }
+
+
+        private DefaultResourceRepository<TodoItem> GetRepository()
+        {
+
+            var contextResolverMock = new Mock<IDbContextResolver>();
+            _contextMock
+                .Setup(m => m.Set<TodoItem>())
+                .Returns(_dbSetMock.Object);
+
+            contextResolverMock
+                .Setup(m => m.GetContext())
+                .Returns(_contextMock.Object);
+
+            var resourceGraph = new ResourceGraphBuilder().AddResource<TodoItem>().Build();
+
+
+            return new DefaultResourceRepository<TodoItem>(
+                _targetedFieldsMock.Object,
+                _contextResolverMock.Object,
+                resourceGraph, null, null);
         }
     }
 }
