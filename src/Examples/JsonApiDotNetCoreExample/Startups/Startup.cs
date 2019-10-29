0bf496be419efa038ce2a9958eb87cf32d5a5f3d
Maurits Moeys <maurits.moeys@gmail.com>
2019-10-29 17:26:01 +0100

    fix: CI builds

--- src/Examples/JsonApiDotNetCoreExample/Startups/Startup.cs
+++ src/Examples/JsonApiDotNetCoreExample/Startups/Startup.cs
@@ -44,6 +44,7 @@ namespace JsonApiDotNetCoreExample
                     options.LoaDatabaseValues = true;
                 },
                 discovery => discovery.AddCurrentAssembly());
+            services.AddClientSerialization();
         }
 
         public virtual void Configure(
