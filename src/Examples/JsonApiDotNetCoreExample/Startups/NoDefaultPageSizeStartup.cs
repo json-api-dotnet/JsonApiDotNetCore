7fb1c5e62bba66363c2e1f543e43b51fe3974e8b
Maurits Moeys <maurits.moeys@gmail.com>
2019-10-30 17:28:53 +0100

    test: reproduce bug

---
+++ src/Examples/JsonApiDotNetCoreExample/Startups/NoDefaultPageSizeStartup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using JsonApiDotNetCoreExample.Data;
+using Microsoft.EntityFrameworkCore;
+using JsonApiDotNetCore.Extensions;
+using System.Reflection;
+
+namespace JsonApiDotNetCoreExample
+{
+    /// <summary>
+    /// This should be in JsonApiDotNetCoreExampleTests project but changes in .net core 3.0
+    /// do no longer allow that. See https://github.com/aspnet/AspNetCore/issues/15373.
+    /// </summary>
+    public class NoDefaultPageSizeStartup : Startup
+    {
+        public NoDefaultPageSizeStartup(IWebHostEnvironment env)
+        : base(env)
+        { }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            var loggerFactory = new LoggerFactory();
+            var mvcBuilder = services.AddMvcCore();
+            services
+                .AddSingleton<ILoggerFactory>(loggerFactory)
+                .AddLogging(builder =>
+                {
+                    builder.AddConsole();
+                })
+                .AddDbContext<AppDbContext>(options => options.UseNpgsql(GetDbConnectionString()), ServiceLifetime.Transient)
+                .AddJsonApi(options => {
+                    options.Namespace = "api/v1";
+                    options.IncludeTotalRecordCount = true;
+                    options.EnableResourceHooks = true;
+                    options.LoaDatabaseValues = true;
+                    options.AllowClientGeneratedIds = true;
+                },
+                discovery => discovery.AddAssembly(Assembly.Load(nameof(JsonApiDotNetCoreExample))),
+                mvcBuilder: mvcBuilder);
+        }
+    }
+}
