4cc91ca9f29158900608d5cd1529c7f29affd9aa
Maurits Moeys <maurits.moeys@gmail.com>
2019-11-06 11:30:13 +0100

    fix: discovery unit test with ServiceProvider instead of ScopedServiceProvider

--- test/UnitTests/ResourceHooks/DiscoveryTests.cs
+++ test/UnitTests/ResourceHooks/DiscoveryTests.cs
@@ -6,8 +6,7 @@ using JsonApiDotNetCore.Builders;
 using JsonApiDotNetCore.Internal;
 using JsonApiDotNetCore.Internal.Contracts;
 using System;
-using Moq;
-using JsonApiDotNetCore.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace UnitTests.ResourceHooks
 {
@@ -22,32 +21,27 @@ namespace UnitTests.ResourceHooks
             public override void AfterDelete(HashSet<Dummy> entities, ResourcePipeline pipeline, bool succeeded) { }
         }
 
-        private IServiceProvider MockProvider(object service)
+        private IServiceProvider MockProvider<TResource>(object service) where TResource : class, IIdentifiable
         {
-            var mock = new Mock<IServiceProvider>();
-            mock.Setup(m => m.GetService(It.IsAny<Type>())).Returns(service);
-            return mock.Object;
+            var services = new ServiceCollection();
+            services.AddScoped((_) => (ResourceDefinition<TResource>)service);
+            return services.BuildServiceProvider();
         }
-
 
         [Fact]
         public void Hook_Discovery()
         {
             // Arrange & act
-            var hookConfig = new HooksDiscovery<Dummy>(MockProvider(new DummyResourceDefinition()));
+            var hookConfig = new HooksDiscovery<Dummy>(MockProvider<Dummy>(new DummyResourceDefinition()));
             // Assert
             Assert.Contains(ResourceHook.BeforeDelete, hookConfig.ImplementedHooks);
             Assert.Contains(ResourceHook.AfterDelete, hookConfig.ImplementedHooks);
-
         }
 
         public class AnotherDummy : Identifiable { }
         public abstract class ResourceDefintionBase<T> : ResourceDefinition<T> where T : class, IIdentifiable
         {
-            public ResourceDefintionBase(IResourceGraph resourceGraph) : base(resourceGraph)
-            {
-            }
-
+            public ResourceDefintionBase(IResourceGraph resourceGraph) : base(resourceGraph) { }
             public override IEnumerable<T> BeforeDelete(IEntityHashSet<T> affected, ResourcePipeline pipeline) { return affected; }
             public override void AfterDelete(HashSet<T> entities, ResourcePipeline pipeline, bool succeeded) { }
         }
@@ -56,16 +50,16 @@ namespace UnitTests.ResourceHooks
         {
             public AnotherDummyResourceDefinition() : base(new ResourceGraphBuilder().AddResource<AnotherDummy>().Build()) { }
         }
+
         [Fact]
         public void Hook_Discovery_With_Inheritance()
         {
             // Arrange & act
-            var hookConfig = new HooksDiscovery<AnotherDummy>(MockProvider(new AnotherDummyResourceDefinition()));
+            var hookConfig = new HooksDiscovery<AnotherDummy>(MockProvider<AnotherDummy>(new AnotherDummyResourceDefinition()));
             // Assert
             Assert.Contains(ResourceHook.BeforeDelete, hookConfig.ImplementedHooks);
             Assert.Contains(ResourceHook.AfterDelete, hookConfig.ImplementedHooks);
         }
-
 
         public class YetAnotherDummy : Identifiable { }
         public class YetAnotherDummyResourceDefinition : ResourceDefinition<YetAnotherDummy>
@@ -77,6 +71,7 @@ namespace UnitTests.ResourceHooks
             [LoadDatabaseValues(false)]
             public override void AfterDelete(HashSet<YetAnotherDummy> entities, ResourcePipeline pipeline, bool succeeded) { }
         }
+
         [Fact]
         public void LoadDatabaseValues_Attribute_Not_Allowed()
         {
@@ -84,7 +79,7 @@ namespace UnitTests.ResourceHooks
             Assert.Throws<JsonApiSetupException>(() =>
             {
                 // Arrange & act
-                var hookConfig = new HooksDiscovery<YetAnotherDummy>(MockProvider(new YetAnotherDummyResourceDefinition()));
+                var hookConfig = new HooksDiscovery<YetAnotherDummy>(MockProvider<YetAnotherDummy>(new YetAnotherDummyResourceDefinition()));
             });
 
         }
