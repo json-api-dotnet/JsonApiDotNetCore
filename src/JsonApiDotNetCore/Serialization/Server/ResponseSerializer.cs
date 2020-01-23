0170a34067485f3c4431ec0eb324d6ae845d9144
Bart Koelman <bart@degreed.com>
2020-01-23 19:06:54 +0100

    Fixed invalid top-level self link. Removed dependency on ResourceContext in the process, because it was no longer needed. Additionally I changed the decision to render which links to be taken from the related entity (from Article in a request like /people/1/articles instead of Person), because I believe that is more correct than taking it from the primary entity, which is the old behavior.

--- src/JsonApiDotNetCore/Serialization/Server/ResponseSerializer.cs
+++ src/JsonApiDotNetCore/Serialization/Server/ResponseSerializer.cs
@@ -40,9 +40,8 @@ namespace JsonApiDotNetCore.Serialization.Server
                                   ILinkBuilder linkBuilder,
                                   IIncludedResourceObjectBuilder includedBuilder,
                                   IFieldsToSerialize fieldsToSerialize,
-                                  IResourceObjectBuilder resourceObjectBuilder,
-                                  IResourceContextProvider provider) :
-            base(resourceObjectBuilder, provider)
+                                  IResourceObjectBuilder resourceObjectBuilder) :
+            base(resourceObjectBuilder)
         {
             _fieldsToSerialize = fieldsToSerialize;
             _linkBuilder = linkBuilder;
@@ -158,7 +157,7 @@ namespace JsonApiDotNetCore.Serialization.Server
         /// </summary>
         private void AddTopLevelObjects(Document document)
         {
-            document.Links = _linkBuilder.GetTopLevelLinks(_provider.GetResourceContext<TResource>());
+            document.Links = _linkBuilder.GetTopLevelLinks();
             document.Meta = _metaBuilder.GetMeta();
             document.Included = _includedBuilder.Build();
         }
