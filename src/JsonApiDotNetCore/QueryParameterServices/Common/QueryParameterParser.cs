6c2a6590a9820e266fc2c7e773f3a94154d63a46
Bart Koelman <bart@degreed.com>
2020-04-07 07:39:46 +0200

    Added logging for query string parsing

--- src/JsonApiDotNetCore/QueryParameterServices/Common/QueryParameterParser.cs
+++ src/JsonApiDotNetCore/QueryParameterServices/Common/QueryParameterParser.cs
@@ -5,6 +5,7 @@ using JsonApiDotNetCore.Controllers;
 using JsonApiDotNetCore.Exceptions;
 using JsonApiDotNetCore.Query;
 using JsonApiDotNetCore.QueryParameterServices.Common;
+using Microsoft.Extensions.Logging;
 
 namespace JsonApiDotNetCore.Services
 {
@@ -14,12 +15,15 @@ namespace JsonApiDotNetCore.Services
         private readonly IJsonApiOptions _options;
         private readonly IRequestQueryStringAccessor _queryStringAccessor;
         private readonly IEnumerable<IQueryParameterService> _queryServices;
+        private ILogger<QueryParameterParser> _logger;
 
-        public QueryParameterParser(IJsonApiOptions options, IRequestQueryStringAccessor queryStringAccessor, IEnumerable<IQueryParameterService> queryServices)
+        public QueryParameterParser(IJsonApiOptions options, IRequestQueryStringAccessor queryStringAccessor, IEnumerable<IQueryParameterService> queryServices, ILoggerFactory loggerFactory)
         {
             _options = options;
             _queryStringAccessor = queryStringAccessor;
             _queryServices = queryServices;
+
+            _logger = loggerFactory.CreateLogger<QueryParameterParser>();
         }
 
         /// <inheritdoc/>
@@ -38,6 +42,8 @@ namespace JsonApiDotNetCore.Services
                 var service = _queryServices.FirstOrDefault(s => s.CanParse(pair.Key));
                 if (service != null)
                 {
+                    _logger.LogDebug($"Query string parameter '{pair.Key}' with value '{pair.Value}' was accepted by {service.GetType().Name}.");
+
                     if (!service.IsEnabled(disableQueryAttribute))
                     {
                         throw new InvalidQueryStringParameterException(pair.Key,
@@ -46,6 +52,7 @@ namespace JsonApiDotNetCore.Services
                     }
 
                     service.Parse(pair.Key, pair.Value);
+                    _logger.LogDebug($"Query string parameter '{pair.Key}' was successfully parsed.");
                 }
                 else if (!_options.AllowCustomQueryStringParameters)
                 {
