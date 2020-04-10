2393bc68dc03b7dbfc96cf92e9bb464864f043f0
Bart Koelman <bart@degreed.com>
2020-04-10 15:18:13 +0200

    Fixed: update deep property on shared settings that were only shallow-copied breaks with concurrent requests

--- src/JsonApiDotNetCore/Extensions/JsonSerializerExtensions.cs
+++ src/JsonApiDotNetCore/Extensions/JsonSerializerExtensions.cs
@@ -9,9 +9,36 @@ namespace JsonApiDotNetCore.Extensions
         {
             jsonSerializer.NullValueHandling = NullValueHandling.Ignore;
 
-            var contractResolver = (DefaultContractResolver)jsonSerializer.ContractResolver;
-            contractResolver.NamingStrategy.ProcessDictionaryKeys = true;
-            contractResolver.NamingStrategy.ProcessExtensionDataNames = true;
+            // JsonSerializer.Create() only performs a shallow copy of the shared settings, so we cannot change properties on its ContractResolver.
+            // But to serialize ErrorMeta.Data correctly, we need to ensure that JsonSerializer.ContractResolver.NamingStrategy.ProcessExtensionDataNames
+            // is set to 'true' while serializing errors.
+            var sharedContractResolver = (DefaultContractResolver)jsonSerializer.ContractResolver;
+
+            jsonSerializer.ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new AlwaysProcessExtensionDataNamingStrategyWrapper(sharedContractResolver.NamingStrategy)
+            };
+        }
+
+        private sealed class AlwaysProcessExtensionDataNamingStrategyWrapper : NamingStrategy
+        {
+            private readonly NamingStrategy _namingStrategy;
+
+            public AlwaysProcessExtensionDataNamingStrategyWrapper(NamingStrategy namingStrategy)
+            {
+                _namingStrategy = namingStrategy ?? new DefaultNamingStrategy();
+            }
+
+            public override string GetExtensionDataName(string name)
+            {
+                // Ignore the value of ProcessExtensionDataNames property on the wrapped strategy (short-circuit).
+                return ResolvePropertyName(name);
+            }
+
+            protected override string ResolvePropertyName(string name)
+            {
+                return _namingStrategy.GetPropertyName(name, false);
+            }
         }
     }
 }
