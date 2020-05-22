8199caef3fdc9b1a63ad95ef1218a03d075da83d
Bart Koelman <bart@degreed.com>
2020-05-22 18:32:46 +0200

    Fixed broken reports example

--- src/Examples/ReportsExample/Startup.cs
+++ src/Examples/ReportsExample/Startup.cs
@@ -17,7 +17,9 @@ namespace ReportsExample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseRouting();
             app.UseJsonApi();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
