30b532fe3388fa2813183855c814a98b873e6a90
maurei <maurits.moeys@gmail.com>
2020-09-30 15:25:59 +0200

    chore: cleanup

--- src/JsonApiDotNetCore/Configuration/ApplicationBuilderExtensions.cs
+++ src/JsonApiDotNetCore/Configuration/ApplicationBuilderExtensions.cs
@@ -30,12 +30,13 @@ namespace JsonApiDotNetCore.Configuration
         public static void UseJsonApi(this IApplicationBuilder builder)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
-            
+
             using var scope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
             var inverseRelationshipResolver = scope.ServiceProvider.GetRequiredService<IInverseRelationships>();
             inverseRelationshipResolver.Resolve();
-            
-            var jsonApiApplicationBuilder =  builder.ApplicationServices.GetRequiredService<IJsonApiApplicationBuilder>();
+
+            var jsonApiApplicationBuilder =
+                builder.ApplicationServices.GetRequiredService<IJsonApiApplicationBuilder>();
             jsonApiApplicationBuilder.ConfigureMvcOptions = options =>
             {
                 var inputFormatter = builder.ApplicationServices.GetRequiredService<IJsonApiInputFormatter>();
@@ -46,91 +47,11 @@ namespace JsonApiDotNetCore.Configuration
 
                 var routingConvention = builder.ApplicationServices.GetRequiredService<IJsonApiRoutingConvention>();
                 options.Conventions.Insert(0, routingConvention);
-                
-                // var interceptor = new MetadataDetailsProviderListInterceptor(options.ModelMetadataDetailsProviders);
-                // var property = typeof(MvcOptions).GetField("<ModelMetadataDetailsProviders>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
-                // property.SetValue(options, interceptor);
 
-                options.ModelValidatorProviders[0] = new JsonApiModelValidationProvider(options.ModelValidatorProviders[0]);
+                options.ModelValidatorProviders.Insert(0, new JsonApiModelValidationProvider());
             };
 
             builder.UseMiddleware<JsonApiMiddleware>();
-        }
-    }
-
-    internal sealed class MetadataDetailsProviderListInterceptor : IList<IMetadataDetailsProvider>
-    {
-        private readonly IList<IMetadataDetailsProvider> _list;
-
-        public MetadataDetailsProviderListInterceptor(IList<IMetadataDetailsProvider> list)
-        {
-            _list = list;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Add(IMetadataDetailsProvider item)
-        {
-            
-            if (item is IBindingMetadataProvider && item is IDisplayMetadataProvider && item is IValidationMetadataProvider)
-            {
-                _list.Add(new JsonApiMetadataProvider(item));
-            }
-            
-            _list.Add(item);
-        }
-
-        public void Clear()
-        {
-            _list.Clear();
-        }
-
-        public bool Contains(IMetadataDetailsProvider item)
-        {
-            return _list.Contains(item);
-        }
-
-        public void CopyTo(IMetadataDetailsProvider[] array, int arrayIndex)
-        {
-            _list.CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(IMetadataDetailsProvider item)
-        {
-            return _list.Remove(item);
-        }
-
-        public int Count => _list.Count;
-        public bool IsReadOnly => _list.IsReadOnly;
-
-        public IEnumerator<IMetadataDetailsProvider> GetEnumerator()
-        {
-            return _list.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public int IndexOf(IMetadataDetailsProvider item)
-        {
-            return _list.IndexOf(item);
-        }
-
-        public void Insert(int index, IMetadataDetailsProvider item)
-        {
-            _list.Insert(index, item);
-        }
-
-        public void RemoveAt(int index)
-        {
-            _list.RemoveAt(index);
-        }
-
-        public IMetadataDetailsProvider this[int index]
-        {
-            get => _list[index];
-            set => _list[index] = value;
         }
     }
 }
--- src/JsonApiDotNetCore/Configuration/JsonApiModelValidationProvider.cs
+++ src/JsonApiDotNetCore/Configuration/JsonApiModelValidationProvider.cs
@@ -1,117 +1,27 @@
-
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Reflection;
 using JsonApiDotNetCore.Resources.Annotations;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace JsonApiDotNetCore.Configuration
 {
-    public sealed class JsonApiModelValidationProvider : IMetadataBasedModelValidatorProvider
+    public sealed class JsonApiModelValidationProvider : IModelValidatorProvider
     {
-        private readonly IMetadataBasedModelValidatorProvider _internalModelValidatorProvider;
-        
-        public JsonApiModelValidationProvider(IModelValidatorProvider internalModelValidatorProvider)
+        private static readonly FieldInfo _validatorMetadataBackingField;
+        static JsonApiModelValidationProvider()
         {
-            _internalModelValidatorProvider = internalModelValidatorProvider as IMetadataBasedModelValidatorProvider ?? throw new ArgumentNullException(nameof(internalModelValidatorProvider));
-        }
+           _validatorMetadataBackingField = typeof(ValidatorItem).GetField("<ValidatorMetadata>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
 
+        }   
+        
         public void CreateValidators(ModelValidatorProviderContext context)
         {
-            var itemsToRemove = new List<ValidatorItem>();
-            
             foreach (var item in context.Results)
             {
                 if (item.ValidatorMetadata.GetType() == typeof(RequiredAttribute))
                 {
-                    // var interceptor = new MetadataDetailsProviderListInterceptor(options.ModelMetadataDetailsProviders);
-                    var property = item.GetType().GetField("<ValidatorMetadata>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
-                    property.SetValue(item, new JsonApiRequiredAttribute());
-                    // itemsToRemove.Add(item);
+                    _validatorMetadataBackingField.SetValue(item, new JsonApiRequiredAttribute());
                 }
-            }
-
-            // foreach (var item in itemsToRemove)
-            // {
-            //     context.Results.Remove(item);
-            // }
-        }
-
-        public bool HasValidators(Type modelType, IList<object> validatorMetadata)
-        {
-            var hasValidators = _internalModelValidatorProvider.HasValidators(modelType, validatorMetadata);
-            return hasValidators;
-        }
-
-
-        private void FuckAround(ValidationContext validationContext)
-        {
-            // var metadata = validationContext.ModelMetadata;
-            // var memberName = metadata.Name;
-            // var container = validationContext.Container;
-            //
-            // var context = new ValidationContext(
-            //     instance: container ?? validationContext.Model ?? _emptyValidationContextInstance,
-            //     serviceProvider: validationContext.ActionContext?.HttpContext?.RequestServices,
-            //     items: null)
-            // {
-            //     DisplayName = metadata.GetDisplayName(),
-            //     MemberName = memberName
-            // };
-        }
-    }
-    
-    public interface IInternalDataAnnotationsMetadataProvider :         IBindingMetadataProvider,
-        IDisplayMetadataProvider,
-        IValidationMetadataProvider { }
-    
-    public sealed class JsonApiMetadataProvider :
-        IBindingMetadataProvider,
-        IDisplayMetadataProvider,
-        IValidationMetadataProvider
-    {
-        private readonly IMetadataDetailsProvider _internalProvider;
-
-        public JsonApiMetadataProvider(IMetadataDetailsProvider internalProvider)
-        {
-            _internalProvider = internalProvider ?? throw new ArgumentNullException(nameof(internalProvider));
-        }
-
-        /// <inheritdoc />
-        public void CreateBindingMetadata(BindingMetadataProviderContext context)
-        {
-            ((IBindingMetadataProvider)_internalProvider).CreateBindingMetadata(context);
-        }
-
-        /// <inheritdoc />
-        public void CreateDisplayMetadata(DisplayMetadataProviderContext context)
-        {
-            ((IDisplayMetadataProvider)_internalProvider).CreateDisplayMetadata(context);
-        }
-
-        /// <inheritdoc />
-        public void CreateValidationMetadata(ValidationMetadataProviderContext context)
-        {
-            ((IValidationMetadataProvider)_internalProvider).CreateValidationMetadata(context);
-            
-            var validatorMedata = context.ValidationMetadata.ValidatorMetadata;
-            var itemsToReplaceIndices = new List<int>(); 
-            
-            for (int i = 0; i < validatorMedata.Count; i++)
-            {
-                if (validatorMedata[i] is RequiredAttribute)
-                {
-                    itemsToReplaceIndices.Add(i);
-                }
-            }
-
-            foreach (var index in itemsToReplaceIndices)
-            {
-                validatorMedata[index] = new JsonApiRequiredAttribute();
             }
         }
     }
