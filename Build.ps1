63cae0bfd8d87379acfdb5a8671721a85f85181c
Bart Koelman <bart@degreed.com>
2020-11-20 13:33:44 +0100

    Updated package version prefix on cibuilds

--- Build.ps1
+++ Build.ps1
@@ -47,7 +47,7 @@ if ($env:APPVEYOR_REPO_TAG -eq $true) {
     }
 }
 else {
-    $packageVersionSuffix="beta1-$revision"
+    $packageVersionSuffix="rc-$revision"
     Write-Output "VERSION-SUFFIX: $packageVersionSuffix"
     Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$packageVersionSuffix"
                           dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$packageVersionSuffix
