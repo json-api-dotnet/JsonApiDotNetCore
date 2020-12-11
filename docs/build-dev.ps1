294f6d1defef407e18b2301f73b6861cff7e2ade
Bart Koelman <bart@degreed.com>
2020-12-11 14:31:31 +0100

    add Google Search Console

--- docs/build-dev.ps1
+++ docs/build-dev.ps1
@@ -9,13 +9,12 @@ dotnet build .. --configuration Release
 Invoke-Expression ./generate-examples.ps1
 
 docfx ./docfx.json
-Copy-Item home/index.html _site/index.html
-Copy-Item home/favicon.ico _site/favicon.ico
-Copy-Item -Recurse home/assets/* _site/styles/
+Get-ChildItem -Path home/* -Recurse | Copy-Item -Destination _site
 
 cd _site
 httpserver &
 Start-Process "http://localhost:8080/"
+cd ..
 
 Write-Host ""
 Write-Host "Web server started. Press Enter to close."
