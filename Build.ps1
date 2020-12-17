b6dc6ee85a55f01154c67f3b2296b0e7dacc4d2d
Bart Koelman <bart@degreed.com>
2020-12-17 16:00:04 +0100

    update cibuild version

--- Build.ps1
+++ Build.ps1
@@ -47,7 +47,7 @@ if ($env:APPVEYOR_REPO_TAG -eq $true) {
     }
 }
 else {
-    $packageVersionSuffix="rc-$revision"
+    $packageVersionSuffix="pre-$revision"
     Write-Output "VERSION-SUFFIX: $packageVersionSuffix"
     Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$packageVersionSuffix"
                           dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$packageVersionSuffix
