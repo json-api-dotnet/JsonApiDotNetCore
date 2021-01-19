1d6065bb68a40a6c2264e0bd6ae1d1d92bceb1bb
Bart Koelman <bart@degreed.com>
2021-01-19 14:24:44 +0100

    Fixed: resource-level meta in atomic:operation responses

---
+++ test/JsonApiDotNetCoreExampleTests/IntegrationTests/AtomicOperations/Meta/AtomicResourceMetaTests.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using JsonApiDotNetCore.Resources;
+using JsonApiDotNetCore.Serialization.Objects;
+using JsonApiDotNetCoreExample;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace JsonApiDotNetCoreExampleTests.IntegrationTests.AtomicOperations.Meta
+{
+    public sealed class AtomicResourceMetaTests
+        : IClassFixture<IntegrationTestContext<TestableStartup<OperationsDbContext>, OperationsDbContext>>
+    {
+        private readonly IntegrationTestContext<TestableStartup<OperationsDbContext>, OperationsDbContext> _testContext;
+        private readonly OperationsFakers _fakers = new OperationsFakers();
+
+        public AtomicResourceMetaTests(IntegrationTestContext<TestableStartup<OperationsDbContext>, OperationsDbContext> testContext)
+        {
+            _testContext = testContext;
+
+            testContext.ConfigureServicesAfterStartup(services =>
+            {
+                var part = new AssemblyPart(typeof(EmptyStartup).Assembly);
+                services.AddMvcCore().ConfigureApplicationPartManager(apm => apm.ApplicationParts.Add(part));
+
+                services.AddScoped<IResourceDefinition<MusicTrack, Guid>, MusicTrackDefinition>();
+                services.AddScoped<IResourceDefinition<TextLanguage, Guid>, TextLanguageDefinition>();
+            });
+        }
+
+        [Fact]
+        public async Task Returns_resource_meta_in_create_resource_with_side_effects()
+        {
+            // Arrange
+            var newTitle1 = _fakers.MusicTrack.Generate().Title;
+            var newTitle2 = _fakers.MusicTrack.Generate().Title;
+
+            var requestBody = new
+            {
+                atomic__operations = new[]
+                {
+                    new
+                    {
+                        op = "add",
+                        data = new
+                        {
+                            type = "musicTracks",
+                            attributes = new
+                            {
+                                title = newTitle1,
+                                releasedAt = 1.January(2018)
+                            }
+                        }
+                    },
+                    new
+                    {
+                        op = "add",
+                        data = new
+                        {
+                            type = "musicTracks",
+                            attributes = new
+                            {
+                                title = newTitle2,
+                                releasedAt = 23.August(1994)
+                            }
+                        }
+                    }
+                }
+            };
+
+            var route = "/operations";
+
+            // Act
+            var (httpResponse, responseDocument) = await _testContext.ExecutePostAtomicAsync<AtomicOperationsDocument>(route, requestBody);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.OK);
+
+            responseDocument.Results.Should().HaveCount(2);
+
+            responseDocument.Results[0].SingleData.Meta.Should().HaveCount(1);
+            responseDocument.Results[0].SingleData.Meta["Copyright"].Should().Be("(C) 2018. All rights reserved.");
+
+            responseDocument.Results[1].SingleData.Meta.Should().HaveCount(1);
+            responseDocument.Results[1].SingleData.Meta["Copyright"].Should().Be("(C) 1994. All rights reserved.");
+        }
+
+        [Fact]
+        public async Task Returns_top_level_meta_in_update_resource_with_side_effects()
+        {
+            // Arrange
+            var existingLanguage = _fakers.TextLanguage.Generate();
+
+            await _testContext.RunOnDatabaseAsync(async dbContext =>
+            {
+                dbContext.TextLanguages.Add(existingLanguage);
+                await dbContext.SaveChangesAsync();
+            });
+
+            var requestBody = new
+            {
+                atomic__operations = new[]
+                {
+                    new
+                    {
+                        op = "update",
+                        data = new
+                        {
+                            type = "textLanguages",
+                            id = existingLanguage.StringId,
+                            attributes = new
+                            {
+                            }
+                        }
+                    }
+                }
+            };
+
+            var route = "/operations";
+
+            // Act
+            var (httpResponse, responseDocument) = await _testContext.ExecutePostAtomicAsync<AtomicOperationsDocument>(route, requestBody);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.OK);
+
+            responseDocument.Results.Should().HaveCount(1);
+            responseDocument.Results[0].SingleData.Meta.Should().HaveCount(1);
+            responseDocument.Results[0].SingleData.Meta["Notice"].Should().Be("See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for ISO 639-1 language codes.");
+        }
+    }
+}
