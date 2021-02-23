49c18ad40d46ab19944f96bdaa144995ce825e9e
Bart Koelman <bart@degreed.com>
2021-02-23 13:56:31 +0100

    Corrected link rendering for hosting inside an IIS application vdir (#955)
    Corrected link rendering for hosting inside an IIS application virtual directory

---
+++ test/JsonApiDotNetCoreExampleTests/IntegrationTests/HostingInIIS/HostingFakers.cs
@@ -0,0 +1,22 @@
+using System;
+using Bogus;
+using TestBuildingBlocks;
+
+namespace JsonApiDotNetCoreExampleTests.IntegrationTests.HostingInIIS
+{
+    internal sealed class HostingFakers : FakerContainer
+    {
+        private readonly Lazy<Faker<ArtGallery>> _lazyArtGalleryFaker = new Lazy<Faker<ArtGallery>>(() =>
+            new Faker<ArtGallery>()
+                .UseSeed(GetFakerSeed())
+                .RuleFor(artGallery => artGallery.Theme, f => f.Lorem.Word()));
+
+        private readonly Lazy<Faker<Painting>> _lazyPaintingFaker = new Lazy<Faker<Painting>>(() =>
+            new Faker<Painting>()
+                .UseSeed(GetFakerSeed())
+                .RuleFor(painting => painting.Title, f => f.Lorem.Sentence()));
+
+        public Faker<ArtGallery> ArtGallery => _lazyArtGalleryFaker.Value;
+        public Faker<Painting> Painting => _lazyPaintingFaker.Value;
+    }
+}
---
+++ test/JsonApiDotNetCoreExampleTests/IntegrationTests/HostingInIIS/PaintingsController.cs
@@ -0,0 +1,19 @@
+using JsonApiDotNetCore.Configuration;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Controllers.Annotations;
+using JsonApiDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JsonApiDotNetCoreExampleTests.IntegrationTests.HostingInIIS
+{
+    [DisableRoutingConvention, Route("custom/path/to/paintings")]
+    public sealed class PaintingsController : JsonApiController<Painting>
+    {
+        public PaintingsController(IJsonApiOptions options, ILoggerFactory loggerFactory,
+            IResourceService<Painting> resourceService)
+            : base(options, loggerFactory, resourceService)
+        {
+        }
+    }
+}
