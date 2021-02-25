379dbcfcbbcfe7ecd1eaddd79a9d6aa1863aa292
Bart Koelman <bart@degreed.com>
2021-02-25 11:27:13 +0100

    extra: UsedImplicitly on models

--- test/JsonApiDotNetCoreExampleTests/IntegrationTests/QueryStrings/LabelColor.cs
+++ test/JsonApiDotNetCoreExampleTests/IntegrationTests/QueryStrings/LabelColor.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
+
 namespace JsonApiDotNetCoreExampleTests.IntegrationTests.QueryStrings
 {
+    [UsedImplicitly(ImplicitUseTargetFlags.Members)]
     public enum LabelColor
     {
         Red,
