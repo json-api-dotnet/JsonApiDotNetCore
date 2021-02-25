1792982b0b3f9bfeb4a333d7b66cdb04f84a8efa
Bart Koelman <bart@degreed.com>
2021-02-25 12:04:40 +0100

    Added formatter directive to ErrorDocumentTests to prevent array chop

--- test/UnitTests/Internal/ErrorDocumentTests.cs
+++ test/UnitTests/Internal/ErrorDocumentTests.cs
@@ -8,11 +8,13 @@ namespace UnitTests.Internal
 {
     public sealed class ErrorDocumentTests
     {
+        // @formatter:wrap_array_initializer_style wrap_if_long
         [Theory]
         [InlineData(new[] { HttpStatusCode.UnprocessableEntity }, HttpStatusCode.UnprocessableEntity)]
         [InlineData(new[] { HttpStatusCode.UnprocessableEntity, HttpStatusCode.UnprocessableEntity }, HttpStatusCode.UnprocessableEntity)]
         [InlineData(new[] { HttpStatusCode.UnprocessableEntity, HttpStatusCode.Unauthorized }, HttpStatusCode.BadRequest)]
         [InlineData(new[] { HttpStatusCode.UnprocessableEntity, HttpStatusCode.BadGateway }, HttpStatusCode.InternalServerError)]
+        // @formatter:wrap_array_initializer_style restore
         public void ErrorDocument_GetErrorStatusCode_IsCorrect(HttpStatusCode[] errorCodes, HttpStatusCode expected)
         {
             // Arrange
