605889ab2bbdf7287d22d109da8c361d3ca833ef
Bart Koelman <bart@degreed.com>
2021-04-08 13:02:01 +0200

    cibuild matrix (Windows and Linux)

--- Build.ps1
+++ Build.ps1
@@ -1,19 +1,8 @@
-# Gets the version suffix from the repo tag
-# example: v1.0.0-preview1-final => preview1-final
-function Get-Version-Suffix-From-Tag {
-  $tag=$env:APPVEYOR_REPO_TAG_NAME
-  $split=$tag -split "-"
-  $suffix=$split[1..2]
-  $final=$suffix -join "-"
-  return $final
-}
-
 function CheckLastExitCode {
     param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
 
     if ($SuccessCodes -notcontains $LastExitCode) {
-        $msg = "EXE RETURNED EXIT CODE $LastExitCode"
-        throw $msg
+        throw "Executable returned exit code $LastExitCode"
     }
 }
 
@@ -65,7 +54,9 @@ function RunCleanupCode {
 
 function ReportCodeCoverage {
     if ($env:APPVEYOR) {
-        dotnet codecov -f "**\coverage.cobertura.xml"
+        if ($IsWindows) {
+            dotnet codecov -f "**\coverage.cobertura.xml"
+        }
     }
     else {
         dotnet reportgenerator -reports:**\coverage.cobertura.xml -targetdir:artifacts\coverage
@@ -74,8 +65,33 @@ function ReportCodeCoverage {
     CheckLastExitCode
 }
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+function CreateNuGetPackage {
+    if ($env:APPVEYOR_REPO_TAG -eq $true) {
+        # Get the version suffix from the repo tag. Example: v1.0.0-preview1-final => preview1-final
+        $segments = $env:APPVEYOR_REPO_TAG_NAME -split "-"
+        $suffixSegments = $segments[1..2]
+        $versionSuffix = $suffixSegments -join "-"
+    }
+    else {
+        # Get the version suffix from the auto-incrementing build number. Example: "123" => "pre-0123".
+        if ($env:APPVEYOR_BUILD_NUMBER) {
+            $revision = "{0:D4}" -f [convert]::ToInt32($env:APPVEYOR_BUILD_NUMBER, 10)
+            $versionSuffix="pre-$revision"
+        }
+        else {
+            $versionSuffix="pre-0001"
+        }
+    }
+
+    if ([string]::IsNullOrWhitespace($versionSuffix)) {
+        dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts
+    }
+    else {
+        dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$versionSuffix
+    }
+
+    CheckLastExitCode
+}
 
 dotnet tool restore
 CheckLastExitCode
@@ -91,27 +107,4 @@ CheckLastExitCode
 
 ReportCodeCoverage
 
-Write-Output "APPVEYOR_REPO_TAG: $env:APPVEYOR_REPO_TAG"
-
-if ($env:APPVEYOR_REPO_TAG -eq $true) {
-    $revision = Get-Version-Suffix-From-Tag
-    Write-Output "VERSION-SUFFIX: $revision"
-
-    if ([string]::IsNullOrWhitespace($revision)) {
-        Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts"
-                              dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts
-        CheckLastExitCode
-    }
-    else {
-        Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision"
-                              dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$revision
-        CheckLastExitCode
-    }
-}
-else {
-    $packageVersionSuffix="pre-$revision"
-    Write-Output "VERSION-SUFFIX: $packageVersionSuffix"
-    Write-Output "RUNNING dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$packageVersionSuffix"
-                          dotnet pack .\src\JsonApiDotNetCore -c Release -o .\artifacts --version-suffix=$packageVersionSuffix
-    CheckLastExitCode
-}
+CreateNuGetPackage
