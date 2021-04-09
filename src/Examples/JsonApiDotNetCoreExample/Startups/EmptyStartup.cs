2e735e5277bc332c2962e9ac1810c8356cc1472d
Bart Koelman <bart@degreed.com>
2021-04-09 15:15:50 +0200

    Revert comments on EmptyStartup

--- src/Examples/JsonApiDotNetCoreExample/Startups/EmptyStartup.cs
+++ src/Examples/JsonApiDotNetCoreExample/Startups/EmptyStartup.cs
@@ -10,12 +10,10 @@ namespace JsonApiDotNetCoreExample.Startups
     /// </summary>
     public abstract class EmptyStartup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
         public virtual void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
         {
         }
