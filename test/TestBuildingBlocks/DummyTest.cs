783e6f06026ec46963e0f671734d4883e2e0208c
Bart Koelman <bart@degreed.com>
2021-06-14 17:09:25 +0200

    Package updates

---
+++ test/TestBuildingBlocks/DummyTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace TestBuildingBlocks
+{
+    public sealed class DummyTest
+    {
+        [Fact]
+        public void Empty()
+        {
+            // This dummy test exists solely to suppress the warning
+            // during test runs that no tests were found in this project.
+        }
+    }
+}
