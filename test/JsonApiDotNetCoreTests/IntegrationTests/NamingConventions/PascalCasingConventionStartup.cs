0514d56f6c854911f59f1b7e17d4937677e3c26f
Bart Koelman <bart@degreed.com>
2021-09-03 15:18:46 +0200

    Added tests for pascal casing

---
+++ test/JsonApiDotNetCoreTests/IntegrationTests/NamingConventions/PascalCasingConventionStartup.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using JsonApiDotNetCore.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using TestBuildingBlocks;
+
+namespace JsonApiDotNetCoreTests.IntegrationTests.NamingConventions
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+    public sealed class PascalCasingConventionStartup<TDbContext> : TestableStartup<TDbContext>
+        where TDbContext : DbContext
+    {
+        protected override void SetJsonApiOptions(JsonApiOptions options)
+        {
+            base.SetJsonApiOptions(options);
+
+            options.Namespace = "PublicApi";
+            options.UseRelativeLinks = true;
+            options.IncludeTotalResourceCount = true;
+            options.ValidateModelState = true;
+
+            options.SerializerSettings.ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new DefaultNamingStrategy()
+            };
+        }
+    }
+}
---
+++ test/JsonApiDotNetCoreTests/IntegrationTests/NamingConventions/PascalCasingTests.cs
@@ -0,0 +1,204 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using JsonApiDotNetCore.Serialization.Objects;
+using TestBuildingBlocks;
+using Xunit;
+
+namespace JsonApiDotNetCoreTests.IntegrationTests.NamingConventions
+{
+    public sealed class PascalCasingTests : IClassFixture<IntegrationTestContext<PascalCasingConventionStartup<SwimmingDbContext>, SwimmingDbContext>>
+    {
+        private readonly IntegrationTestContext<PascalCasingConventionStartup<SwimmingDbContext>, SwimmingDbContext> _testContext;
+        private readonly SwimmingFakers _fakers = new();
+
+        public PascalCasingTests(IntegrationTestContext<PascalCasingConventionStartup<SwimmingDbContext>, SwimmingDbContext> testContext)
+        {
+            _testContext = testContext;
+
+            testContext.UseController<DivingBoardsController>();
+            testContext.UseController<SwimmingPoolsController>();
+        }
+
+        [Fact]
+        public async Task Can_get_resources_with_include()
+        {
+            // Arrange
+            List<SwimmingPool> pools = _fakers.SwimmingPool.Generate(2);
+            pools[1].DivingBoards = _fakers.DivingBoard.Generate(1);
+
+            await _testContext.RunOnDatabaseAsync(async dbContext =>
+            {
+                await dbContext.ClearTableAsync<SwimmingPool>();
+                dbContext.SwimmingPools.AddRange(pools);
+                await dbContext.SaveChangesAsync();
+            });
+
+            const string route = "/PublicApi/SwimmingPools?include=DivingBoards";
+
+            // Act
+            (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecuteGetAsync<Document>(route);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.OK);
+
+            responseDocument.ManyData.Should().HaveCount(2);
+            responseDocument.ManyData.Should().OnlyContain(resourceObject => resourceObject.Type == "SwimmingPools");
+            responseDocument.ManyData.Should().OnlyContain(resourceObject => resourceObject.Attributes.ContainsKey("IsIndoor"));
+            responseDocument.ManyData.Should().OnlyContain(resourceObject => resourceObject.Relationships.ContainsKey("WaterSlides"));
+            responseDocument.ManyData.Should().OnlyContain(resourceObject => resourceObject.Relationships.ContainsKey("DivingBoards"));
+
+            responseDocument.Included.Should().HaveCount(1);
+            responseDocument.Included[0].Type.Should().Be("DivingBoards");
+            responseDocument.Included[0].Id.Should().Be(pools[1].DivingBoards[0].StringId);
+            responseDocument.Included[0].Attributes["HeightInMeters"].As<decimal>().Should().BeApproximately(pools[1].DivingBoards[0].HeightInMeters);
+            responseDocument.Included[0].Relationships.Should().BeNull();
+            responseDocument.Included[0].Links.Self.Should().Be($"/PublicApi/DivingBoards/{pools[1].DivingBoards[0].StringId}");
+
+            responseDocument.Meta["TotalResources"].Should().Be(2);
+        }
+
+        [Fact]
+        public async Task Can_filter_secondary_resources_with_sparse_fieldset()
+        {
+            // Arrange
+            SwimmingPool pool = _fakers.SwimmingPool.Generate();
+            pool.WaterSlides = _fakers.WaterSlide.Generate(2);
+            pool.WaterSlides[0].LengthInMeters = 1;
+            pool.WaterSlides[1].LengthInMeters = 5;
+
+            await _testContext.RunOnDatabaseAsync(async dbContext =>
+            {
+                dbContext.SwimmingPools.Add(pool);
+                await dbContext.SaveChangesAsync();
+            });
+
+            string route = $"/PublicApi/SwimmingPools/{pool.StringId}/WaterSlides" +
+                "?filter=greaterThan(LengthInMeters,'1')&fields[WaterSlides]=LengthInMeters";
+
+            // Act
+            (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecuteGetAsync<Document>(route);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.OK);
+
+            responseDocument.ManyData.Should().HaveCount(1);
+            responseDocument.ManyData[0].Type.Should().Be("WaterSlides");
+            responseDocument.ManyData[0].Id.Should().Be(pool.WaterSlides[1].StringId);
+            responseDocument.ManyData[0].Attributes.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task Can_create_resource()
+        {
+            // Arrange
+            SwimmingPool newPool = _fakers.SwimmingPool.Generate();
+
+            var requestBody = new
+            {
+                data = new
+                {
+                    type = "SwimmingPools",
+                    attributes = new Dictionary<string, object>
+                    {
+                        ["IsIndoor"] = newPool.IsIndoor
+                    }
+                }
+            };
+
+            const string route = "/PublicApi/SwimmingPools";
+
+            // Act
+            (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.Created);
+
+            responseDocument.SingleData.Should().NotBeNull();
+            responseDocument.SingleData.Type.Should().Be("SwimmingPools");
+            responseDocument.SingleData.Attributes["IsIndoor"].Should().Be(newPool.IsIndoor);
+
+            int newPoolId = int.Parse(responseDocument.SingleData.Id);
+            string poolLink = $"{route}/{newPoolId}";
+
+            responseDocument.SingleData.Relationships.Should().NotBeEmpty();
+            responseDocument.SingleData.Relationships["WaterSlides"].Links.Self.Should().Be($"{poolLink}/relationships/WaterSlides");
+            responseDocument.SingleData.Relationships["WaterSlides"].Links.Related.Should().Be($"{poolLink}/WaterSlides");
+            responseDocument.SingleData.Relationships["DivingBoards"].Links.Self.Should().Be($"{poolLink}/relationships/DivingBoards");
+            responseDocument.SingleData.Relationships["DivingBoards"].Links.Related.Should().Be($"{poolLink}/DivingBoards");
+
+            await _testContext.RunOnDatabaseAsync(async dbContext =>
+            {
+                SwimmingPool poolInDatabase = await dbContext.SwimmingPools.FirstWithIdAsync(newPoolId);
+
+                poolInDatabase.IsIndoor.Should().Be(newPool.IsIndoor);
+            });
+        }
+
+        [Fact]
+        public async Task Applies_casing_convention_on_error_stack_trace()
+        {
+            // Arrange
+            const string requestBody = "{ \"data\": {";
+
+            const string route = "/PublicApi/SwimmingPools";
+
+            // Act
+            (HttpResponseMessage httpResponse, ErrorDocument responseDocument) = await _testContext.ExecutePostAsync<ErrorDocument>(route, requestBody);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.UnprocessableEntity);
+
+            responseDocument.Errors.Should().HaveCount(1);
+
+            Error error = responseDocument.Errors[0];
+            error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            error.Title.Should().Be("Failed to deserialize request body.");
+            error.Meta.Data.Should().ContainKey("StackTrace");
+        }
+
+        [Fact]
+        public async Task Applies_casing_convention_on_source_pointer_from_ModelState()
+        {
+            // Arrange
+            DivingBoard existingBoard = _fakers.DivingBoard.Generate();
+
+            await _testContext.RunOnDatabaseAsync(async dbContext =>
+            {
+                dbContext.DivingBoards.Add(existingBoard);
+                await dbContext.SaveChangesAsync();
+            });
+
+            var requestBody = new
+            {
+                data = new
+                {
+                    type = "DivingBoards",
+                    id = existingBoard.StringId,
+                    attributes = new Dictionary<string, object>
+                    {
+                        ["HeightInMeters"] = -1
+                    }
+                }
+            };
+
+            string route = $"/PublicApi/DivingBoards/{existingBoard.StringId}";
+
+            // Act
+            (HttpResponseMessage httpResponse, ErrorDocument responseDocument) = await _testContext.ExecutePatchAsync<ErrorDocument>(route, requestBody);
+
+            // Assert
+            httpResponse.Should().HaveStatusCode(HttpStatusCode.UnprocessableEntity);
+
+            responseDocument.Errors.Should().HaveCount(1);
+
+            Error error = responseDocument.Errors[0];
+            error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+            error.Title.Should().Be("Input validation failed.");
+            error.Detail.Should().Be("The field HeightInMeters must be between 1 and 20.");
+            error.Source.Pointer.Should().Be("/data/attributes/HeightInMeters");
+        }
+    }
+}
