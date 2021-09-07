0487b7384e0e801eeb074fe7667b507cb83d931b
Bart Koelman <bart@degreed.com>
2021-09-07 17:58:20 +0200

    Removed re-indent of response body on HTTP status code mismatch in tests, because we already use indenting in TestableStartup, so this is no longer needed.

--- test/TestBuildingBlocks/HttpResponseMessageExtensions.cs
+++ test/TestBuildingBlocks/HttpResponseMessageExtensions.cs
@@ -1,11 +1,8 @@
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Primitives;
 using JetBrains.Annotations;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace TestBuildingBlocks
 {
@@ -32,32 +29,11 @@ namespace TestBuildingBlocks
             {
                 if (Subject.StatusCode != statusCode)
                 {
-                    string responseText = GetFormattedContentAsync(Subject).Result;
+                    string responseText = Subject.Content.ReadAsStringAsync().Result;
                     Subject.StatusCode.Should().Be(statusCode, $"response body returned was:\n{responseText}");
                 }
 
                 return new AndConstraint<HttpResponseMessageAssertions>(this);
-            }
-
-            private static async Task<string> GetFormattedContentAsync(HttpResponseMessage responseMessage)
-            {
-                string text = await responseMessage.Content.ReadAsStringAsync();
-
-                try
-                {
-                    if (text.Length > 0)
-                    {
-                        return JsonConvert.DeserializeObject<JObject>(text)?.ToString();
-                    }
-                }
-#pragma warning disable AV1210 // Catch a specific exception instead of Exception, SystemException or ApplicationException
-                catch
-#pragma warning restore AV1210 // Catch a specific exception instead of Exception, SystemException or ApplicationException
-                {
-                    // ignored
-                }
-
-                return text;
             }
         }
     }
