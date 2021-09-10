dff594441e099ffb70f495ed7539fd4c4332bda8
Bart Koelman <bart@degreed.com>
2021-09-10 20:12:23 +0200

    Updated PostgreSQL  to v13.4 (#1072)
    * Updated PostgreSQL to v13.4 and log its version.
    Consolidated shell invocations into one, which is a bit faster.
    
    * Applied suggestion from https://help.appveyor.com/discussions/problems/30239-postgres-fails-to-connect-after-version-change

--- run-docker-postgres.ps1
+++ run-docker-postgres.ps1
@@ -9,4 +9,4 @@ docker run --rm --name jsonapi-dotnet-core-testing       `
  -e POSTGRES_USER=postgres                               `
  -e POSTGRES_PASSWORD=postgres                           `
  -p 5432:5432                                            `
- postgres:12.0
+ postgres:13.4
