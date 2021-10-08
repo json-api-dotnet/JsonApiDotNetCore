3d98057253dc49ccc3267d342f56f549f3f820a5
Bart Koelman <bart@degreed.com>
2021-10-08 12:54:45 +0200

    Opened up ResponseModelAdapter for extensibility

--- src/JsonApiDotNetCore/Serialization/Response/ResponseModelAdapter.cs
+++ src/JsonApiDotNetCore/Serialization/Response/ResponseModelAdapter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text.Json.Serialization;
+using JetBrains.Annotations;
 using JsonApiDotNetCore.AtomicOperations;
 using JsonApiDotNetCore.Configuration;
 using JsonApiDotNetCore.Middleware;
@@ -17,7 +18,8 @@ using JsonApiDotNetCore.Serialization.Objects;
 namespace JsonApiDotNetCore.Serialization.Response
 {
     /// <inheritdoc />
-    public sealed class ResponseModelAdapter : IResponseModelAdapter
+    [PublicAPI]
+    public class ResponseModelAdapter : IResponseModelAdapter
     {
         private static readonly CollectionConverter CollectionConverter = new();
 
@@ -108,7 +110,7 @@ namespace JsonApiDotNetCore.Serialization.Response
             return document;
         }
 
-        private AtomicResultObject ConvertOperation(OperationContainer operation, IImmutableSet<IncludeElementExpression> includeElements,
+        protected virtual AtomicResultObject ConvertOperation(OperationContainer operation, IImmutableSet<IncludeElementExpression> includeElements,
             IncludedCollection includedCollection)
         {
             ResourceObject resourceObject = null;
@@ -129,7 +131,7 @@ namespace JsonApiDotNetCore.Serialization.Response
             };
         }
 
-        private ResourceObject ConvertResource(IIdentifiable resource, ResourceType resourceType, EndpointKind requestKind,
+        protected virtual ResourceObject ConvertResource(IIdentifiable resource, ResourceType resourceType, EndpointKind requestKind,
             IImmutableSet<IncludeElementExpression> includeElements, IncludedCollection includedCollection, bool isInclude)
         {
             IImmutableSet<ResourceFieldAttribute> fieldSet = null;
@@ -160,7 +162,8 @@ namespace JsonApiDotNetCore.Serialization.Response
             return resourceObject;
         }
 
-        private IDictionary<string, object> ConvertAttributes(IIdentifiable resource, ResourceType resourceType, IImmutableSet<ResourceFieldAttribute> fieldSet)
+        protected virtual IDictionary<string, object> ConvertAttributes(IIdentifiable resource, ResourceType resourceType,
+            IImmutableSet<ResourceFieldAttribute> fieldSet)
         {
             if (fieldSet != null)
             {
@@ -198,7 +201,7 @@ namespace JsonApiDotNetCore.Serialization.Response
             return null;
         }
 
-        private IDictionary<string, RelationshipObject> ConvertRelationships(IIdentifiable resource, ResourceType resourceType,
+        protected virtual IDictionary<string, RelationshipObject> ConvertRelationships(IIdentifiable resource, ResourceType resourceType,
             IImmutableSet<ResourceFieldAttribute> fieldSet, EndpointKind requestKind, IImmutableSet<IncludeElementExpression> includeElements,
             IncludedCollection includedCollection)
         {
@@ -244,7 +247,7 @@ namespace JsonApiDotNetCore.Serialization.Response
             return default;
         }
 
-        private RelationshipObject ConvertRelationship(RelationshipAttribute relationship, IIdentifiable leftResource, EndpointKind requestKind,
+        protected virtual RelationshipObject ConvertRelationship(RelationshipAttribute relationship, IIdentifiable leftResource, EndpointKind requestKind,
             IncludeElementExpression includeElement, IncludedCollection includedCollection)
         {
             SingleOrManyData<ResourceIdentifierObject> data = default;
@@ -288,7 +291,7 @@ namespace JsonApiDotNetCore.Serialization.Response
                 };
         }
 
-        private JsonApiObject GetApiObject()
+        protected virtual JsonApiObject GetApiObject()
         {
             if (!_options.IncludeJsonApiVersion)
             {
@@ -311,7 +314,7 @@ namespace JsonApiDotNetCore.Serialization.Response
             return jsonApiObject;
         }
 
-        private IList<ResourceObject> GetIncluded(IncludedCollection includedCollection)
+        protected virtual IList<ResourceObject> GetIncluded(IncludedCollection includedCollection)
         {
             if (includedCollection.ResourceObjects.Any())
             {
@@ -319,76 +322,6 @@ namespace JsonApiDotNetCore.Serialization.Response
             }
 
             return _requestQueryStringAccessor.Query.ContainsKey("include") ? Array.Empty<ResourceObject>() : null;
-        }
-
-        private sealed class IncludedCollection
-        {
-            private readonly List<ResourceObject> _includes = new();
-            private readonly Dictionary<IIdentifiable, int> _resourceToIncludeIndexMap = new(IdentifiableComparer.Instance);
-
-            public IList<ResourceObject> ResourceObjects => _includes;
-
-            public ResourceObject AddOrUpdate(IIdentifiable resource, ResourceObject resourceObject)
-            {
-                if (!_resourceToIncludeIndexMap.ContainsKey(resource))
-                {
-                    _includes.Add(resourceObject);
-                    _resourceToIncludeIndexMap.Add(resource, _includes.Count - 1);
-                }
-                else
-                {
-                    if (resourceObject.Type != null)
-                    {
-                        int existingIndex = _resourceToIncludeIndexMap[resource];
-                        ResourceObject existingVersion = _includes[existingIndex];
-
-                        if (existingVersion != resourceObject)
-                        {
-                            MergeRelationships(resourceObject, existingVersion);
-
-                            return existingVersion;
-                        }
-                    }
-                }
-
-                return resourceObject;
-            }
-
-            private static void MergeRelationships(ResourceObject incomingVersion, ResourceObject existingVersion)
-            {
-                // The code below handles the case where one resource is added through different include chains with different relationships.
-                // We enrich the existing resource object with the added relationships coming from the second chain, to ensure correct resource linkage.
-                //
-                // This is best explained using an example. Consider the next inclusion chains:
-                //
-                // 1. reviewer.loginAttempts
-                // 2. author.preferences
-                // 
-                // Where the relationships `reviewer` and `author` are of the same resource type `people`. Then the next rules apply:
-                //
-                // A. People that were included as reviewers from inclusion chain (1) should come with their `loginAttempts` included, but not those from chain (2).
-                // B. People that were included as authors from inclusion chain (2) should come with their `preferences` included, but not those from chain (1).
-                // C. For a person that was included as both an reviewer and author (i.e. targeted by both chains), both `loginAttempts` and `preferences` need
-                //    to be present.
-                //
-                // For rule (C), the related resources will be included as usual, but we need to fix resource linkage here by merging the relationship objects.
-                //
-                // Note that this implementation breaks the overall depth-first ordering of included objects. So solve that, we'd need to use a dependency graph
-                // for included objects instead of a flat list, which may affect performance. Since the ordering is not guaranteed anyway, keeping it simple for now.
-
-                foreach ((string relationshipName, RelationshipObject relationshipObject) in existingVersion.Relationships.EmptyIfNull())
-                {
-                    if (!relationshipObject.Data.IsAssigned)
-                    {
-                        SingleOrManyData<ResourceIdentifierObject> incomingRelationshipData = incomingVersion.Relationships[relationshipName].Data;
-
-                        if (incomingRelationshipData.IsAssigned)
-                        {
-                            relationshipObject.Data = incomingRelationshipData;
-                        }
-                    }
-                }
-            }
         }
     }
 }
