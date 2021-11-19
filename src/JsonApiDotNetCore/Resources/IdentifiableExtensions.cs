a79f90c76b3a155e5e8c106b838e3bcfbb8c0be1
Bart Koelman <bart@degreed.com>
2021-11-19 12:02:52 +0100

    Extract constant

--- src/JsonApiDotNetCore/Resources/IdentifiableExtensions.cs
+++ src/JsonApiDotNetCore/Resources/IdentifiableExtensions.cs
@@ -6,15 +6,17 @@ namespace JsonApiDotNetCore.Resources
 {
     internal static class IdentifiableExtensions
     {
+        private const string IdPropertyName = nameof(Identifiable<object>.Id);
+
         public static object GetTypedId(this IIdentifiable identifiable)
         {
             ArgumentGuard.NotNull(identifiable, nameof(identifiable));
 
-            PropertyInfo? property = identifiable.GetType().GetProperty(nameof(Identifiable<object>.Id));
+            PropertyInfo? property = identifiable.GetType().GetProperty(IdPropertyName);
 
             if (property == null)
             {
-                throw new InvalidOperationException($"Resource of type '{identifiable.GetType()}' does not have an 'Id' property.");
+                throw new InvalidOperationException($"Resource of type '{identifiable.GetType()}' does not contain a property named '{IdPropertyName}'.");
             }
 
             object? propertyValue = property.GetValue(identifiable);
@@ -26,7 +28,7 @@ namespace JsonApiDotNetCore.Resources
 
                 if (Equals(propertyValue, defaultValue))
                 {
-                    throw new InvalidOperationException($"Property '{identifiable.GetType().Name}.{nameof(Identifiable<object>.Id)}' should " +
+                    throw new InvalidOperationException($"Property '{identifiable.GetType().Name}.{IdPropertyName}' should " +
                         $"have been assigned at this point, but it contains its default {property.PropertyType.Name} value '{propertyValue}'.");
                 }
             }
