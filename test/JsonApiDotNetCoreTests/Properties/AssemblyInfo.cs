56f621c4d585c34014fb6c1bef91dc98e74b382b
Bart Koelman <bart@degreed.com>
2021-12-07 19:26:36 +0100

    Tryout: parallellize all (fails randomly on my machine)

--- test/JsonApiDotNetCoreTests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using Xunit;
-
-// Puts all test classes in the same collection, meaning they won't be run concurrently.
-// There is no way in xUnit to throttle the number of async tests, which is needed to
-// prevent PostgreSQL failing due to too many open connections at the same time.
-// https://github.com/xunit/xunit/issues/2003
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
