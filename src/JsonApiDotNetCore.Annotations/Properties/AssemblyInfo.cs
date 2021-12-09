9fcaa2241ead4b24d0d8abc33a9f07d177038090
Bart Koelman <bart@degreed.com>
2021-12-09 13:22:51 +0100

    Fixes after merge

--- src/JsonApiDotNetCore.Annotations/Properties/AssemblyInfo.cs
+++ src/JsonApiDotNetCore.Annotations/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Benchmarks")]
 [assembly: InternalsVisibleTo("JsonApiDotNetCore")]
+[assembly: InternalsVisibleTo("JsonApiDotNetCore.OpenApi")]
 [assembly: InternalsVisibleTo("JsonApiDotNetCoreTests")]
 [assembly: InternalsVisibleTo("UnitTests")]
 [assembly: InternalsVisibleTo("TestBuildingBlocks")]
