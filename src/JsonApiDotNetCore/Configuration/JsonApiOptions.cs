02d19b5ecf71b5cf9652425b87ff61ae659ef773
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2022-08-04 00:22:05 +0200

    Adapts marking many-to-many relationships as tracked so it no longer needs the Issue26779 EF Core back-compat switch. The switch is unavailable in EF 7, so this change unblocks us from upgrade.

--- src/JsonApiDotNetCore/Configuration/JsonApiOptions.cs
+++ src/JsonApiDotNetCore/Configuration/JsonApiOptions.cs
@@ -102,12 +102,6 @@ public sealed class JsonApiOptions : IJsonApiOptions
         }
     };
 
-    static JsonApiOptions()
-    {
-        // Bug workaround for https://github.com/dotnet/efcore/issues/27436
-        AppContext.SetSwitch("Microsoft.EntityFrameworkCore.Issue26779", true);
-    }
-
     public JsonApiOptions()
     {
         _lazySerializerReadOptions = new Lazy<JsonSerializerOptions>(() => new JsonSerializerOptions(SerializerOptions), LazyThreadSafetyMode.PublicationOnly);
