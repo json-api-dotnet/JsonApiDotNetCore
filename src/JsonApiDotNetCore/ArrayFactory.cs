9cb57ee0bf48448e233737ad9721ff36f01ccedd
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2022-08-22 00:17:33 +0200

    Remove #pragma

--- src/JsonApiDotNetCore/ArrayFactory.cs
+++ src/JsonApiDotNetCore/ArrayFactory.cs
@@ -1,6 +1,3 @@
-#pragma warning disable AV1008 // Class should not be static
-#pragma warning disable AV1130 // Return type in method signature should be an interface to an unchangeable collection
-
 namespace JsonApiDotNetCore;
 
 internal static class ArrayFactory
