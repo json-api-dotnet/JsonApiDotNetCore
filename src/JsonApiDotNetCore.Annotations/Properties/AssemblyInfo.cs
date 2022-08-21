d2078a54b0c38d0b5abea4687b19109e5ed64c50
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2022-08-21 23:53:46 +0200

    Remove InternalsVisibleTo and nested .editorconfig

--- src/JsonApiDotNetCore.Annotations/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Benchmarks")]
-[assembly: InternalsVisibleTo("JsonApiDotNetCore")]
-[assembly: InternalsVisibleTo("JsonApiDotNetCoreTests")]
-[assembly: InternalsVisibleTo("UnitTests")]
-[assembly: InternalsVisibleTo("TestBuildingBlocks")]
--- src/JsonApiDotNetCore/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Benchmarks")]
-[assembly: InternalsVisibleTo("JsonApiDotNetCoreTests")]
-[assembly: InternalsVisibleTo("UnitTests")]
-[assembly: InternalsVisibleTo("DiscoveryTests")]
-[assembly: InternalsVisibleTo("TestBuildingBlocks")]
