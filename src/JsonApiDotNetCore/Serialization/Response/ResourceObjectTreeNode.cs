damien-rousseau/JsonApiDotNetCore_Polymorphism

Basic plumbing of version through the pipeline

src/JsonApiDotNetCore/Serialization/Response/ResourceObjectTreeNode.cs
@@ -268,12 +268,12 @@ namespace JsonApiDotNetCore.Serialization.Response
                     return false;
                 }
 
-                return x.Type == y.Type && x.Id == y.Id && x.Lid == y.Lid;
+                return x.Type == y.Type && x.Id == y.Id && x.Lid == y.Lid && x.Version == y.Version;
             }
 
             public int GetHashCode(ResourceObject obj)
             {
-                return HashCode.Combine(obj.Type, obj.Id, obj.Lid);
+                return HashCode.Combine(obj.Type, obj.Id, obj.Lid, obj.Version);
             }
         }
     }

json-api-dotnet/JsonApiDotNetCore

src/JsonApiDotNetCore/Serialization/Response/ResourceObjectTreeNode.cs
@@ -265,12 +265,12 @@ internal sealed class ResourceObjectTreeNode : IEquatable<ResourceObjectTreeNode
                 return false;
             }
 
-            return left.Type == right.Type && left.Id == right.Id && left.Lid == right.Lid;
+            return left.Type == right.Type && left.Id == right.Id && left.Lid == right.Lid && left.Version == right.Version;
         }
 
         public int GetHashCode(ResourceObject obj)
         {
-            return HashCode.Combine(obj.Type, obj.Id, obj.Lid);
+            return HashCode.Combine(obj.Type, obj.Id, obj.Lid, obj.Version);
         }
     }
 }