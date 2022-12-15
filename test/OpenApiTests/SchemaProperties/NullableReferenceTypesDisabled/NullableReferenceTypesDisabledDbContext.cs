d7c8b4b04190cec2b900be40bd68e5c0746e272a
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2022-12-15 11:40:56 +0100

    Post-merge fixes

--- test/OpenApiTests/SchemaProperties/NullableReferenceTypesDisabled/NullableReferenceTypesDisabledDbContext.cs
+++ test/OpenApiTests/SchemaProperties/NullableReferenceTypesDisabled/NullableReferenceTypesDisabledDbContext.cs
@@ -1,10 +1,11 @@
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using TestBuildingBlocks;
 
 namespace OpenApiTests.SchemaProperties.NullableReferenceTypesDisabled;
 
 [UsedImplicitly(ImplicitUseTargetFlags.Members)]
-public sealed class NullableReferenceTypesDisabledDbContext : DbContext
+public sealed class NullableReferenceTypesDisabledDbContext : TestableDbContext
 {
     public DbSet<Chicken> Chicken => Set<Chicken>();
 
