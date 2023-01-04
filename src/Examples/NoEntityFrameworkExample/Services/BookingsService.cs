85fbc37c89b0fd5d3a22967f0af90b1ab1c5bc2e
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-01-04 13:53:59 +0100

    Fix for issue 1238

--- src/Examples/NoEntityFrameworkExample/Services/BookingsService.cs
+++ src/Examples/NoEntityFrameworkExample/Services/BookingsService.cs
@@ -11,15 +11,22 @@ namespace NoEntityFrameworkExample.Services;
 // This is the implementation of BookingService.
 public class BookingsService : JsonApiResourceService<Booking, string>
 {
+    private readonly IQueryLayerComposer _queryLayerComposer;
+    private readonly IJsonApiRequest _request;
+
     public BookingsService(IResourceRepositoryAccessor repositoryAccessor, IQueryLayerComposer queryLayerComposer, IPaginationContext paginationContext,
         IJsonApiOptions options, ILoggerFactory loggerFactory, IJsonApiRequest request, IResourceChangeTracker<Booking> resourceChangeTracker,
         IResourceDefinitionAccessor resourceDefinitionAccessor)
         : base(repositoryAccessor, queryLayerComposer, paginationContext, options, loggerFactory, request, resourceChangeTracker, resourceDefinitionAccessor)
     {
+        _queryLayerComposer = queryLayerComposer;
+        _request = request;
     }
 
     public override async Task<IReadOnlyCollection<Booking>> GetAsync(CancellationToken cancellationToken)
     {
+        _queryLayerComposer.ComposeFromConstraints(_request.PrimaryResourceType!);
+
         return new List<Booking>
         {
             new()
