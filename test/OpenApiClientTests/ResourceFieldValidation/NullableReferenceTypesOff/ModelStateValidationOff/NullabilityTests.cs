d33723089fcdb81e03724744127bf496a4aa4676
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-01-08 23:03:44 +0100

    Add relationship nullability assertions in OpenAPI client tests

--- test/OpenApiClientTests/ResourceFieldValidation/NullableReferenceTypesOff/ModelStateValidationOff/NullabilityTests.cs
+++ test/OpenApiClientTests/ResourceFieldValidation/NullableReferenceTypesOff/ModelStateValidationOff/NullabilityTests.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using FluentAssertions;
 using OpenApiClientTests.ResourceFieldValidation.NullableReferenceTypesOff.ModelStateValidationOff.GeneratedCode;
+using TestBuildingBlocks;
 using Xunit;
 
 namespace OpenApiClientTests.ResourceFieldValidation.NullableReferenceTypesOff.ModelStateValidationOff;
@@ -14,10 +15,31 @@ public sealed class NullabilityTests
     [InlineData(nameof(ResourceAttributesInPostRequest.RequiredValueType), NullabilityState.NotNull)]
     [InlineData(nameof(ResourceAttributesInPostRequest.NullableValueType), NullabilityState.Nullable)]
     [InlineData(nameof(ResourceAttributesInPostRequest.RequiredNullableValueType), NullabilityState.Nullable)]
-    public void Nullability_of_generated_property_is_as_expected(string propertyName, NullabilityState expectedState)
+    public void Nullability_of_generated_attribute_property_is_as_expected(string propertyName, NullabilityState expectedState)
     {
-        PropertyInfo[] properties = typeof(ResourceAttributesInPostRequest).GetProperties();
-        PropertyInfo property = properties.Single(property => property.Name == propertyName);
+        // Act
+        PropertyInfo? property = typeof(ResourceAttributesInPostRequest).GetProperty(propertyName);
+
+        // Assert
+        property.ShouldNotBeNull();
         property.Should().HaveNullabilityState(expectedState);
+    }
+
+    [Theory]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.ToOne), NullabilityState.Unknown)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.RequiredToOne), NullabilityState.Unknown)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.ToMany), NullabilityState.Unknown)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.RequiredToMany), NullabilityState.Unknown)]
+    public void Nullability_of_generated_relationship_property_is_as_expected(string propertyName, NullabilityState expectedState)
+    {
+        // Act
+        PropertyInfo? relationshipProperty = typeof(ResourceRelationshipsInPostRequest).GetProperty(propertyName);
+
+        // Assert
+        relationshipProperty.ShouldNotBeNull();
+
+        PropertyInfo? dataProperty = relationshipProperty.PropertyType.GetProperty("Data");
+        dataProperty.ShouldNotBeNull();
+        dataProperty.Should().HaveNullabilityState(expectedState);
     }
 }
--- test/OpenApiClientTests/ResourceFieldValidation/NullableReferenceTypesOn/ModelStateValidationOff/NullabilityTests.cs
+++ test/OpenApiClientTests/ResourceFieldValidation/NullableReferenceTypesOn/ModelStateValidationOff/NullabilityTests.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using FluentAssertions;
 using OpenApiClientTests.ResourceFieldValidation.NullableReferenceTypesOn.ModelStateValidationOff.GeneratedCode;
+using TestBuildingBlocks;
 using Xunit;
 
 namespace OpenApiClientTests.ResourceFieldValidation.NullableReferenceTypesOn.ModelStateValidationOff;
@@ -16,10 +17,33 @@ public sealed class NullabilityTests
     [InlineData(nameof(ResourceAttributesInPostRequest.RequiredValueType), NullabilityState.NotNull)]
     [InlineData(nameof(ResourceAttributesInPostRequest.NullableValueType), NullabilityState.Nullable)]
     [InlineData(nameof(ResourceAttributesInPostRequest.RequiredNullableValueType), NullabilityState.Nullable)]
-    public void Nullability_of_generated_property_is_as_expected(string propertyName, NullabilityState expectedState)
+    public void Nullability_of_generated_attribute_property_is_as_expected(string propertyName, NullabilityState expectedState)
     {
-        PropertyInfo[] properties = typeof(ResourceAttributesInPostRequest).GetProperties();
-        PropertyInfo property = properties.Single(property => property.Name == propertyName);
+        // Act
+        PropertyInfo? property = typeof(ResourceAttributesInPostRequest).GetProperty(propertyName);
+
+        // Assert
+        property.ShouldNotBeNull();
         property.Should().HaveNullabilityState(expectedState);
+    }
+
+    [Theory]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.NonNullableToOne), NullabilityState.NotNull)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.RequiredNonNullableToOne), NullabilityState.NotNull)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.NullableToOne), NullabilityState.Nullable)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.RequiredNullableToOne), NullabilityState.Nullable)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.ToMany), NullabilityState.NotNull)]
+    [InlineData(nameof(ResourceRelationshipsInPostRequest.RequiredToMany), NullabilityState.NotNull)]
+    public void Nullability_of_generated_relationship_property_is_as_expected(string propertyName, NullabilityState expectedState)
+    {
+        // Act
+        PropertyInfo? relationshipProperty = typeof(ResourceRelationshipsInPostRequest).GetProperty(propertyName);
+
+        // Assert
+        relationshipProperty.ShouldNotBeNull();
+
+        PropertyInfo? dataProperty = relationshipProperty.PropertyType.GetProperty("Data");
+        dataProperty.ShouldNotBeNull();
+        dataProperty.Should().HaveNullabilityState(expectedState);
     }
 }
