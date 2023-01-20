79cfb3df29044ac9826b7a9b41bccb654815525e
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-01-20 02:46:19 +0100

    Improve error response from invalid ModelState when [ApiController] is used

--- test/JsonApiDotNetCoreTests/IntegrationTests/CustomRoutes/ApiControllerAttributeTests.cs
+++ test/JsonApiDotNetCoreTests/IntegrationTests/CustomRoutes/ApiControllerAttributeTests.cs
@@ -35,4 +35,48 @@ public sealed class ApiControllerAttributeTests : IClassFixture<IntegrationTestC
         error.Links.ShouldNotBeNull();
         error.Links.About.Should().Be("https://tools.ietf.org/html/rfc7231#section-6.5.4");
     }
+
+    [Fact]
+    public async Task ProblemDetails_from_invalid_ModelState_is_translated_into_error_response()
+    {
+        // Arrange
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "civilians",
+                attributes = new
+                {
+                    name = (string?)null,
+                    yearOfBirth = 1850
+                }
+            }
+        };
+
+        const string route = "/world-civilians";
+
+        // Act
+        (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.BadRequest);
+
+        responseDocument.Errors.ShouldHaveCount(2);
+
+        ErrorObject error1 = responseDocument.Errors[0];
+        error1.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error1.Links.ShouldNotBeNull();
+        error1.Links.About.Should().Be("https://tools.ietf.org/html/rfc7231#section-6.5.1");
+        error1.Title.Should().Be("One or more validation errors occurred.");
+        error1.Detail.Should().Be("The Name field is required.");
+        error1.Source.Should().BeNull();
+
+        ErrorObject error2 = responseDocument.Errors[1];
+        error2.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error2.Links.ShouldNotBeNull();
+        error2.Links.About.Should().Be("https://tools.ietf.org/html/rfc7231#section-6.5.1");
+        error2.Title.Should().Be("One or more validation errors occurred.");
+        error2.Detail.Should().Be("The field YearOfBirth must be between 1900 and 2050.");
+        error2.Source.Should().BeNull();
+    }
 }
--- test/JsonApiDotNetCoreTests/IntegrationTests/CustomRoutes/Civilian.cs
+++ test/JsonApiDotNetCoreTests/IntegrationTests/CustomRoutes/Civilian.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using JetBrains.Annotations;
 using JsonApiDotNetCore.Resources;
 using JsonApiDotNetCore.Resources.Annotations;
@@ -10,4 +11,8 @@ public sealed class Civilian : Identifiable<int>
 {
     [Attr]
     public string Name { get; set; } = null!;
+
+    [Attr]
+    [Range(1900, 2050)]
+    public int YearOfBirth { get; set; }
 }
