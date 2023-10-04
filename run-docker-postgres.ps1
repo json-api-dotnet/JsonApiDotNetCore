da8562ae998989fe26d66d1843b4cd85bbbf6bc8
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-10-04 11:30:22 +0200

    Add switch for starting pgAdmin in run-docker-postgres

--- run-docker-postgres.ps1
+++ run-docker-postgres.ps1
@@ -1,12 +1,18 @@
 #Requires -Version 7.0
 
-# This script starts a docker container with postgres database, used for running tests.
+# This script starts a PostgreSQL database in a docker container, which is required for running tests locally.
+# When the -UI switch is passed, pgAdmin (a web-based PostgreSQL management tool) is started in a second container, which lets you query the database.
+# To connect to pgAdmin, open http://localhost:5050 and login with user "admin@admin.com", password "postgres". Use hostname "db" when registering the server.
 
-docker container stop jsonapi-dotnet-core-testing
+param(
+    [switch] $UI=$False
+)
 
-docker run --rm --name jsonapi-dotnet-core-testing       `
- -e POSTGRES_DB=JsonApiDotNetCoreExample                 `
- -e POSTGRES_USER=postgres                               `
- -e POSTGRES_PASSWORD=postgres                           `
- -p 5432:5432                                            `
- postgres:15
+docker container stop jsonapi-postgresql-db
+docker container stop jsonapi-postgresql-management
+
+docker run --pull always --rm --detach --name jsonapi-postgresql-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:latest
+
+if ($UI) {
+    docker run --pull always --rm --detach --name jsonapi-postgresql-management --link jsonapi-postgresql-db:db -e PGADMIN_DEFAULT_EMAIL=admin@admin.com -e PGADMIN_DEFAULT_PASSWORD=postgres -p 5050:80 dpage/pgadmin4:latest
+}
