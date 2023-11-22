4a708553ba11d82979fdfa647826bd0fd85abdce
Björn Harrtell <bjorn@wololo.org>
2023-11-22 21:46:43 +0100

    Move to separate unit

--- src/JsonApiDotNetCore/Queries/Parsing/FilterParser.cs
+++ src/JsonApiDotNetCore/Queries/Parsing/FilterParser.cs
@@ -585,20 +585,6 @@ public class FilterParser : QueryExpressionParser, IFilterParser
         }
     }
 
-    /// <summary>
-    /// Converts a constant value within a query string parameter to an <see cref="object" />.
-    /// </summary>
-    /// <param name="value">
-    /// The constant value to convert from.
-    /// </param>
-    /// <param name="position">
-    /// The zero-based position of <paramref name="value" /> in the query string parameter value.
-    /// </param>
-    /// <returns>
-    /// The converted object instance.
-    /// </returns>
-    public delegate object ConstantValueConverter(string value, int position);
-
     private sealed class PopResourceTypeOnDispose : IDisposable
     {
         private readonly Stack<ResourceType> _resourceTypeStack;
