78b54ddd055499f15344de5051a9295ae5330763
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-11-30 12:38:22 +0100

    Promote Global:PropertyCanBeMadeInitOnly to warning, so it aligns with what Resharper shows in VS, and address new violations

--- test/DapperTests/IntegrationTests/FrozenClock.cs
+++ test/DapperTests/IntegrationTests/FrozenClock.cs
@@ -1,8 +1,10 @@
 using DapperExample;
 using FluentAssertions.Extensions;
+using JetBrains.Annotations;
 
 namespace DapperTests.IntegrationTests;
 
+[UsedImplicitly(ImplicitUseTargetFlags.Members)]
 internal sealed class FrozenClock : IClock
 {
     private static readonly DateTimeOffset DefaultTime = 1.January(2020).At(1, 1, 1).AsUtc();
