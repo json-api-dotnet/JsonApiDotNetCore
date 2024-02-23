4d805f1c41f4d252d9441022ae3c1e55edbdcae6
Grégoire <gregoire.verdier@gmail.com>
2024-02-22 19:43:16 -0500

    Update src/JsonApiDotNetCore.OpenApi.Client/ApiResponse.cs
    Co-authored-by: Bart Koelman <10324372+bkoelman@users.noreply.github.com>

--- src/JsonApiDotNetCore.OpenApi.Client/ApiResponse.cs
+++ src/JsonApiDotNetCore.OpenApi.Client/ApiResponse.cs
@@ -7,7 +7,6 @@ namespace JsonApiDotNetCore.OpenApi.Client;
 public class ApiResponse(int statusCode, IReadOnlyDictionary<string, IEnumerable<string>> headers)
 {
     public int StatusCode { get; private set; } = statusCode;
-
     public IReadOnlyDictionary<string, IEnumerable<string>> Headers { get; private set; } = headers;
 
     public static async Task<TResponse?> TranslateAsync<TResponse>(Func<Task<TResponse>> operation)
