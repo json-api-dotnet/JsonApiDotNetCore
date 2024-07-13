db16af1492bea0b218f66f42225b25a86391b8be
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2024-07-13 22:30:36 +0200

    Produce error on invalid ID in request body

--- test/JsonApiDotNetCoreTests/IntegrationTests/ReadWrite/Creating/CreateResourceWithClientGeneratedIdTests.cs
+++ test/JsonApiDotNetCoreTests/IntegrationTests/ReadWrite/Creating/CreateResourceWithClientGeneratedIdTests.cs
@@ -21,6 +21,7 @@ public sealed class CreateResourceWithClientGeneratedIdTests : IClassFixture<Int
 
         testContext.UseController<WorkItemGroupsController>();
         testContext.UseController<RgbColorsController>();
+        testContext.UseController<UserAccountsController>();
 
         testContext.ConfigureServices(services =>
         {
@@ -334,6 +335,231 @@ public sealed class CreateResourceWithClientGeneratedIdTests : IClassFixture<Int
         ErrorObject error = responseDocument.Errors[0];
         error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
         error.Title.Should().Be("Failed to deserialize request body: The 'id' element is required.");
+        error.Detail.Should().BeNull();
+        error.Source.ShouldNotBeNull();
+        error.Source.Pointer.Should().Be("/data");
+        error.Meta.ShouldContainKey("requestBody").With(value => value.ShouldNotBeNull().ToString().ShouldNotBeEmpty());
+    }
+
+    [Theory]
+    [InlineData(ClientIdGenerationMode.Allowed)]
+    [InlineData(ClientIdGenerationMode.Required)]
+    public async Task Cannot_create_resource_with_client_generated_zero_guid_ID(ClientIdGenerationMode mode)
+    {
+        // Arrange
+        var options = (JsonApiOptions)_testContext.Factory.Services.GetRequiredService<IJsonApiOptions>();
+        options.ClientIdGeneration = mode;
+
+        WorkItemGroup newGroup = _fakers.WorkItemGroup.Generate();
+
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "workItemGroups",
+                id = Guid.Empty.ToString(),
+                attributes = new
+                {
+                    name = newGroup.Name
+                }
+            }
+        };
+
+        const string route = "/workItemGroups";
+
+        // Act
+        (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.UnprocessableEntity);
+
+        responseDocument.Errors.ShouldHaveCount(1);
+
+        ErrorObject error = responseDocument.Errors[0];
+        error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        error.Title.Should().Be("Failed to deserialize request body: The 'id' element is invalid.");
+        error.Detail.Should().BeNull();
+        error.Source.ShouldNotBeNull();
+        error.Source.Pointer.Should().Be("/data");
+        error.Meta.ShouldContainKey("requestBody").With(value => value.ShouldNotBeNull().ToString().ShouldNotBeEmpty());
+    }
+
+    [Theory]
+    [InlineData(ClientIdGenerationMode.Allowed)]
+    [InlineData(ClientIdGenerationMode.Required)]
+    public async Task Cannot_create_resource_with_client_generated_empty_guid_ID(ClientIdGenerationMode mode)
+    {
+        // Arrange
+        var options = (JsonApiOptions)_testContext.Factory.Services.GetRequiredService<IJsonApiOptions>();
+        options.ClientIdGeneration = mode;
+
+        WorkItemGroup newGroup = _fakers.WorkItemGroup.Generate();
+
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "workItemGroups",
+                id = string.Empty,
+                attributes = new
+                {
+                    name = newGroup.Name
+                }
+            }
+        };
+
+        const string route = "/workItemGroups";
+
+        // Act
+        (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.UnprocessableEntity);
+
+        responseDocument.Errors.ShouldHaveCount(1);
+
+        ErrorObject error = responseDocument.Errors[0];
+        error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        error.Title.Should().Be("Failed to deserialize request body: The 'id' element is invalid.");
+        error.Detail.Should().BeNull();
+        error.Source.ShouldNotBeNull();
+        error.Source.Pointer.Should().Be("/data");
+        error.Meta.ShouldContainKey("requestBody").With(value => value.ShouldNotBeNull().ToString().ShouldNotBeEmpty());
+    }
+
+    [Theory]
+    [InlineData(ClientIdGenerationMode.Allowed)]
+    [InlineData(ClientIdGenerationMode.Required)]
+    public async Task Can_create_resource_with_client_generated_empty_string_ID(ClientIdGenerationMode mode)
+    {
+        // Arrange
+        var options = (JsonApiOptions)_testContext.Factory.Services.GetRequiredService<IJsonApiOptions>();
+        options.ClientIdGeneration = mode;
+
+        RgbColor newColor = _fakers.RgbColor.Generate();
+
+        await _testContext.RunOnDatabaseAsync(async dbContext =>
+        {
+            await dbContext.ClearTableAsync<RgbColor>();
+        });
+
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "rgbColors",
+                id = string.Empty,
+                attributes = new
+                {
+                    displayName = newColor.DisplayName
+                }
+            }
+        };
+
+        const string route = "/rgbColors?fields[rgbColors]=id";
+
+        // Act
+        (HttpResponseMessage httpResponse, string responseDocument) = await _testContext.ExecutePostAsync<string>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.NoContent);
+
+        responseDocument.Should().BeEmpty();
+
+        await _testContext.RunOnDatabaseAsync(async dbContext =>
+        {
+            RgbColor colorInDatabase = await dbContext.RgbColors.FirstWithIdAsync((string?)string.Empty);
+
+            colorInDatabase.DisplayName.Should().Be(newColor.DisplayName);
+        });
+
+        PropertyInfo? property = typeof(RgbColor).GetProperty(nameof(Identifiable<object>.Id));
+        property.ShouldNotBeNull();
+        property.PropertyType.Should().Be(typeof(string));
+    }
+
+    [Theory]
+    [InlineData(ClientIdGenerationMode.Allowed)]
+    [InlineData(ClientIdGenerationMode.Required)]
+    public async Task Cannot_create_resource_with_client_generated_zero_long_ID(ClientIdGenerationMode mode)
+    {
+        // Arrange
+        var options = (JsonApiOptions)_testContext.Factory.Services.GetRequiredService<IJsonApiOptions>();
+        options.ClientIdGeneration = mode;
+
+        UserAccount newAccount = _fakers.UserAccount.Generate();
+
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "userAccounts",
+                id = "0",
+                attributes = new
+                {
+                    firstName = newAccount.FirstName,
+                    lastName = newAccount.LastName
+                }
+            }
+        };
+
+        const string route = "/userAccounts";
+
+        // Act
+        (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.UnprocessableEntity);
+
+        responseDocument.Errors.ShouldHaveCount(1);
+
+        ErrorObject error = responseDocument.Errors[0];
+        error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        error.Title.Should().Be("Failed to deserialize request body: The 'id' element is invalid.");
+        error.Detail.Should().BeNull();
+        error.Source.ShouldNotBeNull();
+        error.Source.Pointer.Should().Be("/data");
+        error.Meta.ShouldContainKey("requestBody").With(value => value.ShouldNotBeNull().ToString().ShouldNotBeEmpty());
+    }
+
+    [Theory]
+    [InlineData(ClientIdGenerationMode.Allowed)]
+    [InlineData(ClientIdGenerationMode.Required)]
+    public async Task Cannot_create_resource_with_client_generated_empty_long_ID(ClientIdGenerationMode mode)
+    {
+        // Arrange
+        var options = (JsonApiOptions)_testContext.Factory.Services.GetRequiredService<IJsonApiOptions>();
+        options.ClientIdGeneration = mode;
+
+        UserAccount newAccount = _fakers.UserAccount.Generate();
+
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "userAccounts",
+                id = string.Empty,
+                attributes = new
+                {
+                    firstName = newAccount.FirstName,
+                    lastName = newAccount.LastName
+                }
+            }
+        };
+
+        const string route = "/userAccounts";
+
+        // Act
+        (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.UnprocessableEntity);
+
+        responseDocument.Errors.ShouldHaveCount(1);
+
+        ErrorObject error = responseDocument.Errors[0];
+        error.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        error.Title.Should().Be("Failed to deserialize request body: The 'id' element is invalid.");
         error.Detail.Should().BeNull();
         error.Source.ShouldNotBeNull();
         error.Source.Pointer.Should().Be("/data");
