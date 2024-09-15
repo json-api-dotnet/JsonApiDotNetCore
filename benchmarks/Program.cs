5bf36a83a1ba7b5c1da99865c7b725b13270023b
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2024-09-16 00:26:14 +0200

    Fix IDE0210: Convert to top-level statements

--- benchmarks/Program.cs
+++ benchmarks/Program.cs
@@ -3,20 +3,12 @@ using Benchmarks.Deserialization;
 using Benchmarks.QueryString;
 using Benchmarks.Serialization;
 
-namespace Benchmarks;
+var switcher = new BenchmarkSwitcher([
+    typeof(ResourceDeserializationBenchmarks),
+    typeof(OperationsDeserializationBenchmarks),
+    typeof(ResourceSerializationBenchmarks),
+    typeof(OperationsSerializationBenchmarks),
+    typeof(QueryStringParserBenchmarks)
+]);
 
-internal static class Program
-{
-    private static void Main(string[] args)
-    {
-        var switcher = new BenchmarkSwitcher([
-            typeof(ResourceDeserializationBenchmarks),
-            typeof(OperationsDeserializationBenchmarks),
-            typeof(ResourceSerializationBenchmarks),
-            typeof(OperationsSerializationBenchmarks),
-            typeof(QueryStringParserBenchmarks)
-        ]);
-
-        switcher.Run(args);
-    }
-}
+switcher.Run(args);
