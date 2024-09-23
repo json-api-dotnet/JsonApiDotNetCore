a5337ac97a96d1b79446a3db20af447b184db374
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2024-09-23 03:16:08 +0200

    Suppress CA1852: Seal internal types

--- test/OpenApiNSwagClientTests/ResourceFieldValidation/NullableReferenceTypesOff/ModelStateValidationOff/GeneratedCode/NrtOffMsvOffClient.cs
+++ test/OpenApiNSwagClientTests/ResourceFieldValidation/NullableReferenceTypesOff/ModelStateValidationOff/GeneratedCode/NrtOffMsvOffClient.cs
@@ -1,6 +1,8 @@
 using JsonApiDotNetCore.OpenApi.Client.NSwag;
 using Newtonsoft.Json;
 
+#pragma warning disable CA1852 // Seal internal types
+
 namespace OpenApiNSwagClientTests.ResourceFieldValidation.NullableReferenceTypesOff.ModelStateValidationOff.GeneratedCode;
 
 internal partial class NrtOffMsvOffClient : JsonApiClient
--- test/OpenApiNSwagClientTests/ResourceFieldValidation/NullableReferenceTypesOn/ModelStateValidationOn/GeneratedCode/NrtOnMsvOnClient.cs
+++ test/OpenApiNSwagClientTests/ResourceFieldValidation/NullableReferenceTypesOn/ModelStateValidationOn/GeneratedCode/NrtOnMsvOnClient.cs
@@ -1,6 +1,8 @@
 using JsonApiDotNetCore.OpenApi.Client.NSwag;
 using Newtonsoft.Json;
 
+#pragma warning disable CA1852 // Seal internal types
+
 namespace OpenApiNSwagClientTests.ResourceFieldValidation.NullableReferenceTypesOn.ModelStateValidationOn.GeneratedCode;
 
 internal partial class NrtOnMsvOnClient : JsonApiClient
