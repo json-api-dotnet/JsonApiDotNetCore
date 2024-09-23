dca3ba8a8fee50832d7f0217d3dfa856229591a7
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2024-09-23 23:59:31 +0200

    Suppress CA1064: Exceptions should be public

--- src/JsonApiDotNetCore.OpenApi.Client.NSwag/UnreachableCodeException.cs
+++ src/JsonApiDotNetCore.OpenApi.Client.NSwag/UnreachableCodeException.cs
@@ -1,3 +1,5 @@
+#pragma warning disable CA1064 // Exceptions should be public
+
 namespace JsonApiDotNetCore.OpenApi.Client.NSwag;
 
 internal sealed class UnreachableCodeException() : Exception("This code should not be reachable.");
