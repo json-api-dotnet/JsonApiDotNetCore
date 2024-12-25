185f62a908aab2adc10367ee5fdb88cc9fb03ca0
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2024-12-25 10:07:01 +0100

    Delete tests to verify coverage change

--- test/JsonApiDotNetCoreTests/IntegrationTests/AtomicOperations/ResourceDefinitions/SparseFieldSets/LyricPermissionProvider.cs
+++
@@ -1,6 +0,0 @@
-namespace JsonApiDotNetCoreTests.IntegrationTests.AtomicOperations.ResourceDefinitions.SparseFieldSets;
-
-public sealed class LyricPermissionProvider
-{
-    internal bool CanViewText { get; set; }
-}
--- test/JsonApiDotNetCoreTests/IntegrationTests/AtomicOperations/Transactions/ExtraDbContext.cs
+++
@@ -1,9 +0,0 @@
-using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore;
-using TestBuildingBlocks;
-
-namespace JsonApiDotNetCoreTests.IntegrationTests.AtomicOperations.Transactions;
-
-[UsedImplicitly(ImplicitUseTargetFlags.Members)]
-public sealed class ExtraDbContext(DbContextOptions<ExtraDbContext> options)
-    : TestableDbContext(options);
