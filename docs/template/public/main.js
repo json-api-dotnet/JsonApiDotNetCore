eb88cc8860241337d66717ab948a8b35c4a4294e
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2025-05-05 16:37:39 +0200

    Add GitHub icon

---
+++ docs/template/public/main.js
@@ -0,0 +1,11 @@
+// From https://github.com/dotnet/docfx/discussions/9644
+
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/json-api-dotnet/JsonApiDotNetCore',
+      title: 'GitHub'
+    }
+  ]
+}
