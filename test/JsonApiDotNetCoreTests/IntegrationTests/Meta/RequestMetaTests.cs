d34319ec2819ce73a84e5741e499d3c66d107023
Marcos Magalhães <mrmarcosmagalhaes@gmail.com>
2025-07-16 09:35:03 +0100

    test: accepts top level meta in POST relationship request

--- test/JsonApiDotNetCoreTests/IntegrationTests/Meta/RequestMetaTests.cs
+++ test/JsonApiDotNetCoreTests/IntegrationTests/Meta/RequestMetaTests.cs
@@ -172,6 +172,62 @@ public sealed class RequestMetaTests : IClassFixture<IntegrationTestContext<Test
         ValidateMetaData(store.Document.Meta);
     }
 
+    [Fact]
+    public async Task Accepts_top_level_meta_in_post_relationship_request()
+    {
+        // Arrange
+        var store = _testContext.Factory.Services.GetRequiredService<RequestDocumentStore>();
+
+        SupportTicket existingTicket = _fakers.SupportTicket.GenerateOne();
+
+        ProductFamily existingProductFamily = _fakers.ProductFamily.GenerateOne();
+
+        await _testContext.RunOnDatabaseAsync(async dbContext =>
+        {
+            dbContext.ProductFamilies.Add(existingProductFamily);
+            await dbContext.SaveChangesAsync();
+        });
+
+        var requestBody = new
+        {
+            data = new
+            {
+                type = "supportTickets",
+                attributes = new
+                {
+                    description = existingTicket.Description,
+                },
+                relationships = new
+                {
+                    productFamily = new
+                    {
+                        data = new
+                        {
+                            type = "productFamilies",
+                            id = existingProductFamily.StringId
+                        }
+                    }
+                }
+            },
+            meta = GetExampleMetaData()
+        };
+
+        string route = $"/supportTickets";
+
+        // Act
+        (HttpResponseMessage httpResponse, Document responseDocument) = await _testContext.ExecutePostAsync<Document>(route, requestBody);
+
+        // Assert
+        httpResponse.ShouldHaveStatusCode(HttpStatusCode.Created);
+
+        store.Document.Should().NotBeNull();
+        store.Document.Data.SingleValue.Should().NotBeNull();
+        store.Document.Data.SingleValue.Relationships.Should().NotBeNull();
+        store.Document.Data.SingleValue.Relationships.Should().HaveCount(1);
+
+        ValidateMetaData(store.Document.Meta);
+    }
+
     private static Object GetExampleMetaData()
     {
         return new
