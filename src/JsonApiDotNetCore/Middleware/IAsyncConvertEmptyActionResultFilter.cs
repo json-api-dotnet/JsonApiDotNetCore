7b1cc3a2b64b571156c1cf7eecf8ae01eebe080f
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2025-10-21 01:03:08 +0200

    Clarify comments and move to remarks section to keep the namespace list clean (#1818)

--- src/JsonApiDotNetCore/Middleware/IAsyncConvertEmptyActionResultFilter.cs
+++ src/JsonApiDotNetCore/Middleware/IAsyncConvertEmptyActionResultFilter.cs
@@ -5,13 +5,18 @@ namespace JsonApiDotNetCore.Middleware;
 
 /// <summary>
 /// Converts action result without parameters into action result with null parameter.
-/// <example>
-/// <code><![CDATA[
-/// return NotFound() -> return NotFound(null)
-/// ]]></code>
-/// </example>
-/// This ensures our formatter is invoked, where we'll build a JSON:API compliant response. For details, see:
-/// https://github.com/dotnet/aspnetcore/issues/16969
 /// </summary>
+/// <remarks>
+/// This basically turns calls such as
+/// <c>
+/// return NotFound()
+/// </c>
+/// into
+/// <c>
+/// return NotFound(null)
+/// </c>
+/// , so that our formatter is invoked, where we'll build a JSON:API compliant response. For details, see:
+/// https://github.com/dotnet/aspnetcore/issues/16969
+/// </remarks>
 [PublicAPI]
 public interface IAsyncConvertEmptyActionResultFilter : IAsyncAlwaysRunResultFilter;
