131d0f8098915c8cd1b11cfa717a153a3c3e1325
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2025-11-19 03:10:35 +0100

    Adapt to breaking change in dotnet run

--- docs/generate-examples.ps1
+++ docs/generate-examples.ps1
@@ -34,7 +34,7 @@ function Start-WebServer {
     Write-Output "Starting web server"
     $startTimeUtc = Get-Date -AsUTC
     $job = Start-Job -ScriptBlock {
-        dotnet run --project ..\src\Examples\GettingStarted\GettingStarted.csproj --framework net8.0 --configuration Debug --property:TreatWarningsAsErrors=True --urls=http://0.0.0.0:14141
+        dotnet run --project ../src/Examples/GettingStarted/GettingStarted.csproj --framework net8.0 --configuration Debug --property:TreatWarningsAsErrors=True --urls=http://0.0.0.0:14141
     }
 
     $webProcessId = $null
