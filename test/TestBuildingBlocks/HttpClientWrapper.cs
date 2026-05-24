25e321ceeeae5755ef7d499ee77f69fa0d00f7c4
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2026-05-24 09:09:50 +0200

    Extract sending HTTP requests into separate class

---
+++ test/TestBuildingBlocks/HttpClientWrapper.cs
@@ -0,0 +1,124 @@
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using JsonApiDotNetCore.Middleware;
+
+namespace TestBuildingBlocks;
+
+/// <summary>
+/// A wrapper for <see cref="HttpClient" /> that conveniently enables executing HTTP requests against JSON:API endpoints.
+/// </summary>
+public sealed class HttpClientWrapper
+{
+    private static readonly MediaTypeHeaderValue DefaultMediaType = MediaTypeHeaderValue.Parse(JsonApiMediaType.Default.ToString());
+
+    private static readonly MediaTypeWithQualityHeaderValue OperationsMediaType =
+        MediaTypeWithQualityHeaderValue.Parse(JsonApiMediaType.AtomicOperations.ToString());
+
+    private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _serializerOptions;
+
+    public HttpClientWrapper(HttpClient httpClient, JsonSerializerOptions serializerOptions)
+    {
+        ArgumentNullException.ThrowIfNull(httpClient);
+        ArgumentNullException.ThrowIfNull(serializerOptions);
+
+        _httpClient = httpClient;
+        _serializerOptions = serializerOptions;
+    }
+
+    public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteHeadAsync<TResponseDocument>(string requestUrl,
+        Action<HttpRequestHeaders>? setRequestHeaders = null)
+    {
+        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Head, requestUrl, null, null, setRequestHeaders);
+    }
+
+    public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteGetAsync<TResponseDocument>(string requestUrl,
+        Action<HttpRequestHeaders>? setRequestHeaders = null)
+    {
+        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Get, requestUrl, null, null, setRequestHeaders);
+    }
+
+    public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecutePostAsync<TResponseDocument>(string requestUrl,
+        object requestBody, string? contentType = null, Action<HttpRequestHeaders>? setRequestHeaders = null)
+    {
+        MediaTypeHeaderValue mediaType = contentType == null ? DefaultMediaType : MediaTypeHeaderValue.Parse(contentType);
+
+        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Post, requestUrl, requestBody, mediaType, setRequestHeaders);
+    }
+
+    public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecutePostAtomicAsync<TResponseDocument>(string requestUrl,
+        object requestBody)
+    {
+        Action<HttpRequestHeaders> setRequestHeaders = headers => headers.Accept.Add(OperationsMediaType);
+
+        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Post, requestUrl, requestBody, OperationsMediaType, setRequestHeaders);
+    }
+
+    public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecutePatchAsync<TResponseDocument>(string requestUrl,
+        object requestBody, Action<HttpRequestHeaders>? setRequestHeaders = null)
+    {
+        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Patch, requestUrl, requestBody, DefaultMediaType, setRequestHeaders);
+    }
+
+    public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteDeleteAsync<TResponseDocument>(string requestUrl,
+        object? requestBody = null, Action<HttpRequestHeaders>? setRequestHeaders = null)
+    {
+        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Delete, requestUrl, requestBody, DefaultMediaType, setRequestHeaders);
+    }
+
+    private async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteRequestAsync<TResponseDocument>(HttpMethod method,
+        string requestUrl, object? requestBody, MediaTypeHeaderValue? contentType, Action<HttpRequestHeaders>? setRequestHeaders)
+    {
+        using var request = new HttpRequestMessage(method, requestUrl);
+        string? requestText = SerializeRequest(requestBody);
+
+        if (!string.IsNullOrEmpty(requestText))
+        {
+            requestText = requestText.Replace("atomic__", "atomic:");
+            request.Content = new StringContent(requestText);
+            request.Content.Headers.ContentLength = Encoding.UTF8.GetByteCount(requestText);
+
+            if (contentType != null)
+            {
+                request.Content.Headers.ContentType = contentType;
+            }
+        }
+
+        setRequestHeaders?.Invoke(request.Headers);
+
+        HttpResponseMessage responseMessage = await _httpClient.SendAsync(request);
+
+        string responseText = await responseMessage.Content.ReadAsStringAsync();
+        var responseDocument = DeserializeResponse<TResponseDocument>(responseText);
+
+        return (responseMessage, responseDocument!);
+    }
+
+    private string? SerializeRequest(object? requestBody)
+    {
+        return requestBody == null ? null : requestBody as string ?? JsonSerializer.Serialize(requestBody, _serializerOptions);
+    }
+
+    private TResponseDocument? DeserializeResponse<TResponseDocument>(string responseText)
+    {
+        if (typeof(TResponseDocument) == typeof(string))
+        {
+            return (TResponseDocument)(object)responseText;
+        }
+
+        if (string.IsNullOrEmpty(responseText))
+        {
+            return default;
+        }
+
+        try
+        {
+            return JsonSerializer.Deserialize<TResponseDocument>(responseText, _serializerOptions);
+        }
+        catch (JsonException exception)
+        {
+            throw new FormatException($"Failed to deserialize response body to JSON:\n{responseText}", exception);
+        }
+    }
+}
--- test/TestBuildingBlocks/IntegrationTest.cs
+++ test/TestBuildingBlocks/IntegrationTest.cs
@@ -1,8 +1,6 @@
 using System.Net.Http.Headers;
-using System.Text;
 using System.Text.Json;
 using FluentAssertions.Extensions;
-using JsonApiDotNetCore.Middleware;
 using Xunit;
 
 namespace TestBuildingBlocks;
@@ -13,11 +11,6 @@ namespace TestBuildingBlocks;
 /// </summary>
 public abstract class IntegrationTest : IAsyncLifetime
 {
-    private static readonly MediaTypeHeaderValue DefaultMediaType = MediaTypeHeaderValue.Parse(JsonApiMediaType.Default.ToString());
-
-    private static readonly MediaTypeWithQualityHeaderValue OperationsMediaType =
-        MediaTypeWithQualityHeaderValue.Parse(JsonApiMediaType.AtomicOperations.ToString());
-
     private static readonly SemaphoreSlim ThrottleSemaphore = GetDefaultThrottleSemaphore();
 
     public static DateTimeOffset DefaultDateTimeUtc { get; } = 1.January(2020).At(1, 2, 3).AsUtc();
@@ -33,100 +26,52 @@ public abstract class IntegrationTest : IAsyncLifetime
     public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteHeadAsync<TResponseDocument>(string requestUrl,
         Action<HttpRequestHeaders>? setRequestHeaders = null)
     {
-        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Head, requestUrl, null, null, setRequestHeaders);
+        using HttpClient httpClient = CreateClient();
+        var wrapper = new HttpClientWrapper(httpClient, SerializerOptions);
+        return await wrapper.ExecuteHeadAsync<TResponseDocument>(requestUrl, setRequestHeaders);
     }
 
     public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteGetAsync<TResponseDocument>(string requestUrl,
         Action<HttpRequestHeaders>? setRequestHeaders = null)
     {
-        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Get, requestUrl, null, null, setRequestHeaders);
+        using HttpClient httpClient = CreateClient();
+        var wrapper = new HttpClientWrapper(httpClient, SerializerOptions);
+        return await wrapper.ExecuteGetAsync<TResponseDocument>(requestUrl, setRequestHeaders);
     }
 
     public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecutePostAsync<TResponseDocument>(string requestUrl,
         object requestBody, string? contentType = null, Action<HttpRequestHeaders>? setRequestHeaders = null)
     {
-        MediaTypeHeaderValue mediaType = contentType == null ? DefaultMediaType : MediaTypeHeaderValue.Parse(contentType);
-
-        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Post, requestUrl, requestBody, mediaType, setRequestHeaders);
+        using HttpClient httpClient = CreateClient();
+        var wrapper = new HttpClientWrapper(httpClient, SerializerOptions);
+        return await wrapper.ExecutePostAsync<TResponseDocument>(requestUrl, requestBody, contentType, setRequestHeaders);
     }
 
     public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecutePostAtomicAsync<TResponseDocument>(string requestUrl,
         object requestBody)
     {
-        Action<HttpRequestHeaders> setRequestHeaders = headers => headers.Accept.Add(OperationsMediaType);
-
-        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Post, requestUrl, requestBody, OperationsMediaType, setRequestHeaders);
+        using HttpClient httpClient = CreateClient();
+        var wrapper = new HttpClientWrapper(httpClient, SerializerOptions);
+        return await wrapper.ExecutePostAtomicAsync<TResponseDocument>(requestUrl, requestBody);
     }
 
     public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecutePatchAsync<TResponseDocument>(string requestUrl,
         object requestBody, Action<HttpRequestHeaders>? setRequestHeaders = null)
     {
-        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Patch, requestUrl, requestBody, DefaultMediaType, setRequestHeaders);
+        using HttpClient httpClient = CreateClient();
+        var wrapper = new HttpClientWrapper(httpClient, SerializerOptions);
+        return await wrapper.ExecutePatchAsync<TResponseDocument>(requestUrl, requestBody, setRequestHeaders);
     }
 
     public async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteDeleteAsync<TResponseDocument>(string requestUrl,
         object? requestBody = null, Action<HttpRequestHeaders>? setRequestHeaders = null)
     {
-        return await ExecuteRequestAsync<TResponseDocument>(HttpMethod.Delete, requestUrl, requestBody, DefaultMediaType, setRequestHeaders);
-    }
-
-    private async Task<(HttpResponseMessage httpResponse, TResponseDocument responseDocument)> ExecuteRequestAsync<TResponseDocument>(HttpMethod method,
-        string requestUrl, object? requestBody, MediaTypeHeaderValue? contentType, Action<HttpRequestHeaders>? setRequestHeaders)
-    {
-        using var request = new HttpRequestMessage(method, requestUrl);
-        string? requestText = SerializeRequest(requestBody);
-
-        if (!string.IsNullOrEmpty(requestText))
-        {
-            requestText = requestText.Replace("atomic__", "atomic:");
-            request.Content = new StringContent(requestText);
-            request.Content.Headers.ContentLength = Encoding.UTF8.GetByteCount(requestText);
-
-            if (contentType != null)
-            {
-                request.Content.Headers.ContentType = contentType;
-            }
-        }
-
-        setRequestHeaders?.Invoke(request.Headers);
-
-        using HttpClient client = CreateClient();
-        HttpResponseMessage responseMessage = await client.SendAsync(request);
-
-        string responseText = await responseMessage.Content.ReadAsStringAsync();
-        var responseDocument = DeserializeResponse<TResponseDocument>(responseText);
-
-        return (responseMessage, responseDocument!);
-    }
-
-    private string? SerializeRequest(object? requestBody)
-    {
-        return requestBody == null ? null : requestBody as string ?? JsonSerializer.Serialize(requestBody, SerializerOptions);
+        using HttpClient httpClient = CreateClient();
+        var wrapper = new HttpClientWrapper(httpClient, SerializerOptions);
+        return await wrapper.ExecuteDeleteAsync<TResponseDocument>(requestUrl, requestBody, setRequestHeaders);
     }
 
     protected abstract HttpClient CreateClient();
-
-    private TResponseDocument? DeserializeResponse<TResponseDocument>(string responseText)
-    {
-        if (typeof(TResponseDocument) == typeof(string))
-        {
-            return (TResponseDocument)(object)responseText;
-        }
-
-        if (string.IsNullOrEmpty(responseText))
-        {
-            return default;
-        }
-
-        try
-        {
-            return JsonSerializer.Deserialize<TResponseDocument>(responseText, SerializerOptions);
-        }
-        catch (JsonException exception)
-        {
-            throw new FormatException($"Failed to deserialize response body to JSON:\n{responseText}", exception);
-        }
-    }
 
     public async Task InitializeAsync()
     {
