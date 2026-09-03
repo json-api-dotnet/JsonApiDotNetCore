e982cb136beb7667548329ac664c08d9ef77b17f
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2026-09-04 01:47:29 +0200

    Use Docker Compose for starting containers

--- run-docker-postgres.ps1
+++ run-docker-postgres.ps1
@@ -1,18 +1,12 @@
 #Requires -Version 7.0
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 
-# This script starts a PostgreSQL database in a docker container, which is required for running tests locally.
-# When the -UI switch is passed, pgAdmin (a web-based PostgreSQL management tool) is started in a second container, which lets you query the database.
-# To connect to pgAdmin, open http://localhost:5050 and login with user "admin@admin.com", password "postgres". Use hostname "db" when registering the server.
-
-param(
-    [switch] $UI=$false
-)
-
-docker container stop jsonapi-postgresql-db
-docker container stop jsonapi-postgresql-management
-
-docker run --pull always --rm --detach --name jsonapi-postgresql-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:latest -N 500
-
-if ($UI) {
-    docker run --pull always --rm --detach --name jsonapi-postgresql-management --link jsonapi-postgresql-db:db -e PGADMIN_DEFAULT_EMAIL=admin@admin.com -e PGADMIN_DEFAULT_PASSWORD=postgres -p 5050:80 dpage/pgadmin4:latest
+Push-Location $PSScriptRoot
+try {
+    docker compose down
+    docker compose up --detach --wait
+}
+finally {
+    Pop-Location
 }
